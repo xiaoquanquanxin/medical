fffffdf5315482cce62707cc229178bb00eab2c4
xiaoquanquanxin <xxkl123@126.com>
2020-09-27 16:28:56 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="我是首页"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="我是首页"/>
+    </div>
 </template>
-
 <script lang='ts'>
-  // @ is an alias to /src
-  import HelloWorld from '@/components/HelloWorld.vue'
-  
-  export default {
-    name: 'Home',
-    components: {
-      HelloWorld
-    }
-  }
+	// @ is an alias to /src
+	import HelloWorld from '@/components/HelloWorld.vue'
+
+	export default {
+		name: 'Home',
+		components: {
+			HelloWorld
+		}
+	}
 </script>
