888d7c2866f5d01f627190a416837e56781fabcc
xiaoquanquanxin <xxkl123@126.com>
2020-09-28 20:16:50 +0800

    高度

--- src/layout/layoutMain/layoutMain.vue
+++ src/layout/layoutMain/layoutMain.vue
@@ -2,23 +2,29 @@
     <a-layout>
         <LayoutHeader/>
         <LayoutBreadcrumb/>
-        <LayoutContent/>
+        <a-layout-content class="layout-content">
+            <router-view/>
+        </a-layout-content>
     </a-layout>
 </template>
 <script lang='ts'>
 	import {Vue, Component} from 'vue-property-decorator';
 	import LayoutHeader from '@/layout/layoutHeader/layoutHeader.vue';
 	import LayoutBreadcrumb from '@/layout/layoutBreadcrumb/layoutBreadcrumb.vue';
-	import LayoutContent from '@/layout/layoutContent/layoutContent.vue';
 
 	@Component({
 		components: {
 			LayoutHeader,
 			LayoutBreadcrumb,
-			LayoutContent,
 		}
 	})
 	export default class LayoutMain extends Vue {
 
 	}
 </script>
+<style lang="stylus">
+    .layout-content
+        padding: 24px;
+        background: white;
+        max-height: calc(100vh - 50px - 64px);
+</style>
