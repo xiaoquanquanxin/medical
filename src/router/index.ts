aead8a2311d2661044db7474c8fd6319c08d16ba
xiaoquanquanxin <xxkl123@126.com>
2020-09-28 15:51:25 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,52 +1,45 @@
 import Vue from 'vue'
 import VueRouter, {RouteConfig} from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
-
-const routes: Array<RouteConfig> = [
-	{
-		//	首页
-		path: '/',
-		name: 'Home',
-		component: Home,
-	},
-	{
-		//	登陆
-		path: '/login',
-		name: 'Login',
-		component: Login,
-	},
-	{
-		path: '/about',
-		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-	}
-]
-
-const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes
-})
-
 import store from '../store';
-import Login from "@/views/Login.vue"
-// import NProgress from 'nprogress'; // progress bar
-// NProgress.configure({showSpinner: false}); // NProgress Configuration
+import {syncRoutesList, syncRoutesListLength} from "@/router/modules/syncRoutes"
+import {asyncRoutesList} from "@/router/modules/asyncRoutes"
+
+Vue.use(VueRouter);
+const createRouter = (routesList: RouteConfig[]) => new VueRouter({
+	mode: 'hash',
+	routes: routesList.concat(syncRoutesList)
+});
+
+const router = createRouter([]);
 router.beforeEach(async (to, from, next) => {
-	//  去哪儿
-	if (from.path !== to.path) {
+	console.log('来自哪儿', from.path);
+	console.log('去哪儿', to.path);
 
-		// NProgress.done();
-		// return;
+	//	如果没更新路由
+	if (store.state.routeList.routeListLength === syncRoutesListLength) {
+		console.log('拿路由？');
+		//	拿路由
+		const accessRoutes = await store.dispatch('routeList/generateRoutes');
+		const newRouter = createRouter(accessRoutes);
+		// @ts-ignore
+		// router.match = newRouter.match
+		// @ts-ignore
+		// router.matcher = newRouter.matcher;
+		// @ts-ignore
+		router.options.routes = newRouter.options.routes;
+		console.log('拿到路由');
+		await router.addRoutes(accessRoutes);
+		next({...to, replace: true});
+		return;
 	}
-	console.log(from.path, to.path);
-	await store.dispatch('sidebar/setSelectRouteKey', to.path);
-
+	if (to.path === from.path) {
+		return;
+	}
+	console.log('添加路由，元信息是');
+	console.log(to.meta);
+	await store.dispatch('sidebar/setOpenRouteKey', to.meta.openRouteKey);
+	await store.dispatch('sidebar/setSelectRouteKey', to.meta.selectRouteKey);
+	console.log('设置被选中完成了');
 	next();
 })
-export default router
+export default router;
