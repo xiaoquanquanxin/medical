0fa5f5de580eba90224e8eb774a9b7bcfd0c49d9
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 16:42:53 +0800

    高度

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import store from './store'
 import {Button, message} from 'ant-design-vue';
 import Antd from 'ant-design-vue';
 import "./css/reset.css";
-import "./css/layout.css";
-
 import 'ant-design-vue/dist/antd.css';
-
+import "./css/layout.css";
 
 Vue.use(Antd);
 Vue.config.productionTip = false
