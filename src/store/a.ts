c823bcbf02ee838aecc12be7eb0f81f06a0ec4d5
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 19:25:06 +0800

    高度

--- src/store/a.ts
+++ src/store/a.ts
@@ -11,18 +11,18 @@ export const routeMap = {
 	'addDepartment': true,
 	//  编辑科室
 	'editDepartment': true,
-	//  医生管理
-	'doctor': true,
-	//  新增医生
-	'addDoctor': true,
-	//  编辑医生
-	'editDoctor': true,
 	//  疾病管理
 	'disease': true,
 	//  新增疾病
 	'addDisease': true,
 	//  编辑疾病
 	'editDisease': true,
+	//  医生管理
+	'doctor': true,
+	//  新增医生
+	'addDoctor': true,
+	//  编辑医生
+	'editDoctor': true,
 	//  用户管理
 	'user': true,
 	//  新增用户
