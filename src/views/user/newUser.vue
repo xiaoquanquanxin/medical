2b695a242867fa3cf279f44bdb5540574b32df0f
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 20:06:09 +0800

    高度

--- src/views/user/newUser.vue
+++ src/views/user/newUser.vue
@@ -1,19 +1,112 @@
 <template>
-    <div v-if="id">编辑的id ：{{id}}</div>
-    <div v-else>新增用户</div>
+    <div class="layout-content-inner-main">
+        <p>新增医生</p>
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="用户名称">
+                <a-input
+                        v-decorator="userNameDecorator"
+                />
+            </a-form-item>
+            <a-form-item label="性别">
+                <a-select placeholder="请选择地址" default-value="1">
+                    <a-select-option value="1">男</a-select-option>
+                    <a-select-option value="2">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="年龄">
+                <a-input placeholder="请输入年龄"/>
+            </a-form-item>
+            <a-form-item label="身高">
+                <a-input placeholder="请输入身高"/>
+            </a-form-item>
+            <a-form-item label="体重">
+                <a-input placeholder="请输入体重"/>
+            </a-form-item>
+            <a-form-item label="手机号">
+                <a-input placeholder="请输入手机号"/>
+            </a-form-item>
+            <a-form-item label="身份证号">
+                <a-input placeholder="请输入身份证号"/>
+            </a-form-item>
+            <a-form-item label="医院">
+                <a-select placeholder="请选择医院">
+                    <a-select-option value="1">男</a-select-option>
+                    <a-select-option value="2">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="科室">
+                <a-select placeholder="请选择科室">
+                    <a-select-option value="1">男</a-select-option>
+                    <a-select-option value="2">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="主治医生">
+                <a-select placeholder="请选择主治医生">
+                    <a-select-option value="1">男</a-select-option>
+                    <a-select-option value="2">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="营养师">
+                <a-select placeholder="请选择营养师">
+                    <a-select-option value="1">男</a-select-option>
+                    <a-select-option value="2">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="家庭组">
+                <a-input placeholder="请输入家庭组"/>
+            </a-form-item>
+            <a-form-item label="小程序ID">
+                <a-input placeholder="请输入小程序ID"/>
+            </a-form-item>
+            <a-form-item :wrapper-col="{ span: 4, offset: 7 }">
+                <a-button type="primary" html-type="submit">保存</a-button>
+            </a-form-item>
+        </a-form>
+    </div>
 </template>
 <script>
     export default {
-        name: 'UserManagementDetail',
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
         data(){
             return {
-                id: null
+                userId: this.$route.params.userId,
+                //  表单大小
+                formItemLayout: {
+                    labelCol: { span: 6 },
+                    wrapperCol: { offset: 1, span: 8 },
+                },
+
+                //  用户名称规则
+                userNameDecorator: ['userName', {
+                    initialValue: '么脑仁儿',
+                    rules: [{
+                        max: 11,
+                        message: '最多11位',
+                    }, {
+                        required: true,
+                        message: '请输入医院名称'
+                    },]
+                }],
             };
         },
-        created(){
-            const { id } = this.$route.params;
-            console.log(id);
-            this.id = id;
+        methods: {
+            handleSubmit(e){
+                e.preventDefault();
+                this.form.validateFields((err, values) => {
+                    console.log(err);
+                    console.table(values);
+                    if (!err) {
+                        console.log('Received values of form: ', values);
+                    }
+                });
+            },
         }
     };
 </script>
