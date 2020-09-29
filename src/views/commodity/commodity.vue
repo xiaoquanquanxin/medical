fc2078e8cd1dc122f109899f25fb7a90e9df5bc7
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 20:48:54 +0800

    高度

--- src/views/commodity/commodity.vue
+++ src/views/commodity/commodity.vue
@@ -1,9 +1,171 @@
 <template>
-    <p>商品管理</p>
+    <div class="layout-content-inner-main">
+        <p>商品管理</p>
+        <!--搜索相关-->
+        <a-input-group class="a-input-group">
+            <a-row :gutter="8">
+                <a-col :span="5">
+                    <a-input default-value=""/>
+                </a-col>
+                <a-col :span="5">
+                    <a-select default-value="Option1" style="width:100%;">
+                        <a-select-option value="Option1">
+                            Option1
+                        </a-select-option>
+                        <a-select-option value="Option2">
+                            Option2
+                        </a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="5">
+                    <a-button type="primary">
+                        搜索
+                    </a-button>
+                </a-col>
+            </a-row>
+        </a-input-group>
+        <a-input-group class="a-input-group">
+            <a-col :span="5">
+                <a-button type="primary" @click="addcommodity()">
+                    新增商品
+                </a-button>
+            </a-col>
+        </a-input-group>
+        <!--表格-->
+        <a-table
+                :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                :columns="columns"
+                :data-source="data"
+                :scroll="scroll"
+                :pagination="false"
+        >
+            <div slot="tags" slot-scope="scope,sItem,sIndex,extra">
+                <a-space size="small">
+                    <a @click="editCommodity(scope,sItem,sIndex,extra)">编辑</a>
+                    <a @click="onShelf(scope,sItem,sIndex,extra)">上架</a>
+                    <a @click="offShelf(scope,sItem,sIndex,extra)">下架</a>
+                </a-space>
+            </div>
+        </a-table>
+        <!--分页-->
+        <a-row type="flex" justify="end" class="a-input-group">
+            <a-pagination
+                    v-model="pagination.current"
+                    :page-size-options="pagination.pageSizeOptions"
+                    :total="pagination.total"
+                    show-size-changer
+                    :page-size="pagination.pageSize"
+                    @showSizeChange="onShowSizeChange"
+                    @change="pageChange"
+            >
+                <template slot="buildOptionText" slot-scope="props">
+                    <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
+                    <span v-if="props.value === '50'">全部</span>
+                </template>
+            </a-pagination>
+        </a-row>
+    </div>
 </template>
 <script>
+    const columns = [
+        {
+            title: '商品名称',
+            dataIndex: 'commodity',
+            width: 100,
+        },
+        {
+            title: '商品类型',
+            dataIndex: 'city',
+            width: 100,
+        },
+        {
+            title: '场景',
+            dataIndex: 'status',
+            width: 100,
+        },
+        {
+            title: '单位',
+            dataIndex: 'unit',
+            width: 100,
+        },
+        {
+            title: '价格',
+            dataIndex: 'price',
+            width: 100,
+        },
+        {
+            title: '状态',
+            dataIndex: 'icon',
+            width: 100,
+        },
+        {
+            title: '操作',
+            dataIndex: 'tags',
+            key: 'tags',
+            scopedSlots: { customRender: 'tags' },
+        },
+    ];
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push({
+            key: i,
+            commodity: `xx商品`,
+            city: '上海',
+            status: String(i % 2),
+            icon: '商品图标',
+            tags: ['编辑', '关联科室'],
+        });
+    }
+
     export default {
-        name: 'Commodity',
+        data(){
+            return {
+                data,
+                columns,
+                selectedRowKeys: [], // Check here to configure the default column
+                loading: false,
+
+                //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+                scroll: { x: 820, y: 'calc(100vh - 398px)' },
+
+                //  分页信息
+                pagination: {
+                    pageSizeOptions: ['10', '20', '30', '40', '50'],
+                    current: 1,
+                    pageSize: 10,
+                    total: 50,
+                },
+                //  展示莫泰框
+                dialogVisible: false,
+            };
+        },
+        methods: {
+            //  选中表格数据
+            onSelectChange(selectedRowKeys){
+                console.log('selectedRowKeys changed: ', selectedRowKeys);
+                this.selectedRowKeys = selectedRowKeys;
+            },
+            //  展示的每一页数据变换
+            onShowSizeChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+                this.pagination.pageSize = pageSize;
+            },
+            //  切换分页页码
+            pageChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+            },
+            
+            //  编辑商品
+            editCommodity(){
+
+            },
+            //  上架
+            onShelf(){},
+            //  下架
+            offShelf(){}
+        }
     };
 </script>
 <style scoped lang="stylus">
