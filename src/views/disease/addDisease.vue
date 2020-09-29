c8a91ad401b5fc7328b730238b6775cc60e823bd
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 19:22:05 +0800

    高度

---
+++ src/views/disease/addDisease.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="layout-content-inner-main">
+        <p>新增疾病</p>
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="疾病名称">
+                <a-input
+                        v-decorator="diseaseNameDecorator"
+                />
+            </a-form-item>
+            <a-form-item :wrapper-col="{ span: 4, offset: 7 }">
+                <a-button type="primary" html-type="submit">
+                    保存
+                </a-button>
+            </a-form-item>
+        </a-form>
+    </div>
+</template>
+<script>
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        data(){
+            return {
+                //  疾病id
+                diseaseId: this.$route.params.diseaseId,
+                //  表单大小
+                formItemLayout: {
+                    labelCol: { span: 6 },
+                    wrapperCol: { offset: 1, span: 8 },
+                },
+                //  疾病名称
+                diseaseNameDecorator: ['diseaseName', {
+                    initialValue: '普通疾病',
+                    rules: [{
+                        max: 11,
+                        message: '最多11位',
+                    }, {
+                        required: true,
+                        message: '请输入疾病名称'
+                    },]
+                }],
+            };
+        },
+        created(){
+            //  console.log(this.$route.params);
+            console.log('是编辑？', !!this.diseaseId);
+        },
+        methods: {
+            handleSubmit(e){
+                e.preventDefault();
+                this.form.validateFields((err, values) => {
+                    console.log(err);
+                    console.table(values);
+                    if (!err) {
+                        console.log('Received values of form: ', values);
+                    }
+                });
+            },
+        }
+    };
+</script>
+<style></style>
---
+++ src/views/disease/disease.vue
@@ -0,0 +1,223 @@
+<template>
+    <div class="layout-content-inner-main">
+        <p>疾病管理</p>
+        <!--搜索相关-->
+        <a-input-group class="a-input-group">
+            <a-row :gutter="8">
+                <a-col :span="5">
+                    <a-input default-value=""/>
+                </a-col>
+                <a-col :span="5">
+                    <a-select default-value="Option1" style="width:100%;">
+                        <a-select-option value="Option1">
+                            Option1
+                        </a-select-option>
+                        <a-select-option value="Option2">
+                            Option2
+                        </a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="5">
+                    <a-button type="primary">
+                        搜索
+                    </a-button>
+                </a-col>
+            </a-row>
+        </a-input-group>
+        <a-input-group class="a-input-group">
+            <a-col :span="5">
+                <a-button type="primary" @click="addDisease()">
+                    新增疾病
+                </a-button>
+            </a-col>
+        </a-input-group>
+        <!--表格-->
+        <a-table
+                :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                :columns="columns"
+                :data-source="data"
+                :scroll="scroll"
+                :pagination="false"
+        >
+            <div
+                    slot="a-switch"
+                    slot-scope="sItem,sIndex,extra"
+            >
+                <a-switch checked-children="开" un-checked-children="关"
+                          :default-checked="!!sIndex"
+                          @change="aSwitchChange(sItem,sIndex, $event)"
+                />
+            </div>
+            <div slot="tags" slot-scope="scope,sItem,sIndex,extra">
+                <a-space size="small">
+                    <a @click="editDisease(sItem,sIndex,extra)">编辑</a>
+                    <a @click="deleteDisease(sItem)">删除</a>
+                    <a @click="relatedDisease(sItem,sIndex,)">关联疾病</a>
+                </a-space>
+            </div>
+        </a-table>
+        <!--分页-->
+        <a-row type="flex" justify="end" class="a-input-group">
+            <a-pagination
+                    v-model="pagination.current"
+                    :page-size-options="pagination.pageSizeOptions"
+                    :total="pagination.total"
+                    show-size-changer
+                    :page-size="pagination.pageSize"
+                    @showSizeChange="onShowSizeChange"
+                    @change="pageChange"
+            >
+                <template slot="buildOptionText" slot-scope="props">
+                    <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
+                    <span v-if="props.value === '50'">全部</span>
+                </template>
+            </a-pagination>
+        </a-row>
+        <!--莫泰框-->
+        <a-modal v-model="dialogVisible"
+                 :maskClosable="false"
+                 centered
+                 width="800"
+                 title="关联疾病"
+                 ok-text="确认"
+                 cancel-text="取消"
+                 @ok="modalCheck()">
+            <ShuttleBox v-if="dialogVisible"/>
+        </a-modal>
+    </div>
+</template>
+<script>
+    //  关联疾病 穿梭框
+    import ShuttleBox from '@/components/shuttleBox.vue';
+    import { mapGetters, mapActions } from 'vuex';
+    import { dialogMethods } from '../../utils/methods';
+
+    const columns = [
+        {
+            title: '疾病名称',
+            dataIndex: 'disease',
+            width: 100,
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            width: 100,
+            scopedSlots: { customRender: 'a-switch' },
+        },
+        {
+            title: '操作',
+            dataIndex: 'tags',
+            key: 'tags',
+            scopedSlots: { customRender: 'tags' },
+        },
+    ];
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push({
+            key: i,
+            disease: `xx疾病`,
+            status: String(i % 2),
+            tags: ['编辑', '删除', '关联疾病'],
+        });
+    }
+    //  疾病管理
+    export default {
+        components: {
+            ShuttleBox,
+        },
+        computed: {
+            ...mapGetters([
+                'shuttleBox',
+            ]),
+            modalTargetKeys(){
+                return this.$store.state.shuttleBox.modalTargetKeys;
+            }
+        },
+        data(){
+            return {
+                data,
+                columns,
+                selectedRowKeys: [], // Check here to configure the default column
+                loading: false,
+
+                //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+                scroll: {},
+
+                //  分页信息
+                pagination: {
+                    pageSizeOptions: ['10', '20', '30', '40', '50'],
+                    current: 1,
+                    pageSize: 10,
+                    total: 50,
+                },
+                //  展示莫泰框
+                dialogVisible: false,
+            };
+        },
+        mounted(){
+            this.scroll = { x: 820, y: 'calc(100vh - 398px)' };
+        },
+        methods: {
+            ...dialogMethods,
+            //  选中表格数据
+            onSelectChange(selectedRowKeys){
+                console.log('selectedRowKeys changed: ', selectedRowKeys);
+                this.selectedRowKeys = selectedRowKeys;
+            },
+            //  展示的每一页数据变换
+            onShowSizeChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+                this.pagination.pageSize = pageSize;
+            },
+            //  切换分页页码
+            pageChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+            },
+            //  新增疾病
+            addDisease(){
+                console.log('新增疾病');
+                this.$router.push({ path: '/disease/addDisease' });
+            },
+            //  编辑疾病
+            editDisease(sItem, sIndex, extra){
+                this.$router.push({ name: 'editDisease', params: { diseaseId: sIndex } });
+            },
+            //  删除疾病
+            deleteDisease(sItem){
+                console.log(sItem.disease);
+                this.$confirm({
+                    title: `确定删除${sItem.disease}`,
+                    //  content: 'Bla bla ...',
+                    okText: '确认',
+                    cancelText: '取消',
+                    onOk(){
+                        return new Promise((resolve, reject) => {
+                            console.log('发请求');
+                            setTimeout(Math.random() > 0.5 ? resolve : reject, 1111);
+                        }).catch(() => console.log('Oops errors!'));
+                    },
+                    onCancel(){
+                        console.log('取消');
+                    },
+                });
+            },
+            //  关联疾病
+            relatedDisease(sItem, sIndex){
+                console.log(this.dialogVisible);
+                this.showModal();
+                console.log(this.dialogVisible);
+            },
+            //  检查莫泰框的值
+            modalCheck(){
+                console.log(this.modalTargetKeys);
+                console.log('发请求吧');
+                return;
+                this.hideModal();
+            },
+        }
+    };
+</script>
+<style scoped lang="stylus">
+</style>
