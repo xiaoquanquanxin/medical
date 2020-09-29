46b04bb60e7beac291177b83748b3d1c8cdd5fa7
xiaoquanquanxin <xxkl123@126.com>
2020-09-29 16:34:43 +0800

    高度

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,5 +1,6 @@
 const getters = {
 	sidebar: (state: any) => state.app.sidebar,
 	routeList: (state: any) => state.app.routeList,
+	shuttleBox: (state: any) => state.app.shuttleBox,
 };
 export default getters;
