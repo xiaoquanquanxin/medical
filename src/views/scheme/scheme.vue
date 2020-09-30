263f0417c7bec54cc2968e0d67b65e93b84e7e7f
xiaoquanquanxin <xxkl123@126.com>
2020-09-30 17:58:17 +0800

    高度

--- src/views/scheme/scheme.vue
+++ src/views/scheme/scheme.vue
@@ -1,9 +1,158 @@
 <template>
-    <p>公司信息</p>
+    <div class="layout-content-inner-main">
+        <p>方案管理</p>
+        <!--搜索相关-->
+        <a-input-group class="a-input-group">
+            <a-row :gutter="8">
+                <a-col :span="5">
+                    <a-input default-value=""/>
+                </a-col>
+                <a-col :span="5">
+                    <a-select default-value="Option1" style="width:100%;">
+                        <a-select-option value="Option1">
+                            Option1
+                        </a-select-option>
+                        <a-select-option value="Option2">
+                            Option2
+                        </a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="5">
+                    <a-select default-value="Option1" style="width:100%;">
+                        <a-select-option value="Option1">
+                            Option1
+                        </a-select-option>
+                        <a-select-option value="Option2">
+                            Option2
+                        </a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="5">
+                    <a-select default-value="Option1" style="width:100%;">
+                        <a-select-option value="Option1">
+                            Option1
+                        </a-select-option>
+                        <a-select-option value="Option2">
+                            Option2
+                        </a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="3">
+                    <a-button type="primary">
+                        搜索
+                    </a-button>
+                </a-col>
+            </a-row>
+        </a-input-group>
+        <!--表格-->
+        <a-table
+                :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                :columns="columns"
+                :data-source="data"
+                :scroll="scroll"
+                :pagination="false"
+        >
+            <div slot="tags" slot-scope="scope,sItem,sIndex,extra">
+                <a-space size="small">
+                    <a @click="editAccount(sItem,sIndex)">编辑</a>
+                    <a @click="deleteAccount(sItem,sIndex)">删除</a>
+                </a-space>
+            </div>
+        </a-table>
+        <!--分页-->
+        <a-row type="flex" justify="end" class="a-input-group">
+            <a-pagination
+                    v-model="pagination.current"
+                    :page-size-options="pagination.pageSizeOptions"
+                    :total="pagination.total"
+                    show-size-changer
+                    :page-size="pagination.pageSize"
+                    @showSizeChange="onShowSizeChange"
+                    @change="pageChange"
+            >
+                <template slot="buildOptionText" slot-scope="props">
+                    <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
+                    <span v-if="props.value === '50'">全部</span>
+                </template>
+            </a-pagination>
+        </a-row>
+    </div>
 </template>
 <script>
+    import { pagination } from '@/utils/pagination.ts';
+
+    const columns = [
+        {
+            title: '账号',
+            dataIndex: 'hospital',
+            width: 100,
+        },
+        {
+            title: '创建时间',
+            dataIndex: 'city',
+            width: 100,
+        },
+        {
+            title: '创建人',
+            dataIndex: 'status',
+            width: 100,
+        },
+        {
+            title: '角色',
+            dataIndex: 'icon',
+            width: 100,
+        },
+        {
+            title: '操作',
+            dataIndex: 'tags',
+            key: 'tags',
+            scopedSlots: { customRender: 'tags' },
+        },
+    ];
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push({
+            key: i,
+            hospital: `xx医院`,
+            city: '上海',
+            status: String(i % 2),
+            icon: '医院图标',
+            tags: ['编辑', '关联科室'],
+        });
+    }
+
     export default {
-        name: 'CompanyInfo',
+        data(){
+            return {
+                data,
+                columns,
+                selectedRowKeys: [], // Check here to configure the default column
+
+                //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+                scroll: { x: 820, y: 'calc(100vh - 350px)' },
+                //  分页信息
+                pagination,
+
+            };
+        },
+        methods: {
+            //  选中表格数据
+            onSelectChange(selectedRowKeys){
+                console.log('selectedRowKeys changed: ', selectedRowKeys);
+                this.selectedRowKeys = selectedRowKeys;
+            },
+            //  展示的每一页数据变换
+            onShowSizeChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+                this.pagination.pageSize = pageSize;
+            },
+            //  切换分页页码
+            pageChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+            },
+        }
     };
 </script>
 <style scoped lang="stylus">
