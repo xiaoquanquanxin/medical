d5f47052822a7b5b191c30d3229e3bf70ccc924e
xiaoquanquanxin <xxkl123@126.com>
2020-09-30 17:08:38 +0800

    高度

--- src/utils/dialog.js
+++ src/utils/dialog.js
@@ -1,3 +1,12 @@
+//  莫泰框数据
+export const dialogData = {
+    //  是否展示莫泰框
+    visible: false,
+    //  提交按钮loading
+    submitLoading: false,
+};
+
+//  莫泰框方法
 //  开启、关闭莫泰框
 export const dialogMethods = {
     showModal(){
