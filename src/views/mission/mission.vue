8798365d63c3839d3aa188fed7d6ea42e798559f
xiaoquanquanxin <xxkl123@126.com>
2020-09-30 18:29:47 +0800

    高度

--- src/views/mission/mission.vue
+++ src/views/mission/mission.vue
@@ -43,11 +43,117 @@
                 </router-link>
             </a-col>
         </a-input-group>
+        <!--表格-->
+        <a-table
+                :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                :columns="columns"
+                :data-source="data"
+                :scroll="scroll"
+                :pagination="false"
+        >
+            <div slot="tags" slot-scope="scope,sItem,sIndex,extra">
+                <a-space size="small">
+                    <a @click="editMission(sItem,sIndex,extra)">编辑</a>
+                </a-space>
+            </div>
+        </a-table>
+        <!--分页-->
+        <a-row type="flex" justify="end" class="a-input-group">
+            <a-pagination
+                    v-model="pagination.current"
+                    :page-size-options="pagination.pageSizeOptions"
+                    :total="pagination.total"
+                    show-size-changer
+                    :page-size="pagination.pageSize"
+                    @showSizeChange="onShowSizeChange"
+                    @change="pageChange"
+            >
+                <template slot="buildOptionText" slot-scope="props">
+                    <span v-if="props.value !== '50'">{{ props.value }}条/页</span>
+                    <span v-if="props.value === '50'">全部</span>
+                </template>
+            </a-pagination>
+        </a-row>
     </div>
 </template>
 <script>
+    import { towRowSearch } from '../../utils/tableScroll';
+    import { pagination } from '@/utils/pagination.ts';
+
+    const columns = [
+        {
+            title: '文章名称',
+            dataIndex: 'hospital',
+            width: 100,
+        },
+        {
+            title: '阅读量',
+            dataIndex: 'city',
+            width: 100,
+        },
+        {
+            title: '文章类型',
+            dataIndex: 'status',
+            width: 100,
+        },
+        {
+            title: '添加时间',
+            dataIndex: 'icon',
+            width: 100,
+        },
+        {
+            title: '操作',
+            dataIndex: 'tags',
+            key: 'tags',
+            scopedSlots: { customRender: 'tags' },
+        },
+    ];
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push({
+            key: i,
+            hospital: `xx医院`,
+            city: '上海',
+            status: String(i % 2),
+            icon: '医院图标',
+            tags: ['编辑', '关联科室'],
+        });
+    }
     export default {
-    
+        data(){
+            return {
+                data,
+                columns,
+                selectedRowKeys: [], // Check here to configure the default column
+                //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+                scroll: towRowSearch,
+                //  分页信息
+                pagination,
+            };
+        },
+        methods: {
+            //  选中表格数据
+            onSelectChange(selectedRowKeys){
+                console.log('selectedRowKeys changed: ', selectedRowKeys);
+                this.selectedRowKeys = selectedRowKeys;
+            },
+            //  展示的每一页数据变换
+            onShowSizeChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+                this.pagination.pageSize = pageSize;
+            },
+            //  切换分页页码
+            pageChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+            },
+
+            //  编辑
+            editMission(sItem, sIndex){
+                this.$router.push({ name: 'editMission', params: { missionId: 123 } });
+            }
+        }
     };
 </script>
 <style scoped lang="stylus">
