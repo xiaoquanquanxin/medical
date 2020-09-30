211db13176abd1cb5f4ed0b57fe3c826a22b8e90
xiaoquanquanxin <xxkl123@126.com>
2020-09-30 15:01:33 +0800

    高度

--- src/views/hospital/hospital.vue
+++ src/views/hospital/hospital.vue
@@ -102,7 +102,8 @@
     //  关联科室 穿梭框
     import ShuttleBox from '@/components/shuttleBox.vue';
     import { mapGetters, mapActions } from 'vuex';
-    import { dialogMethods } from '../../utils/methods';
+    import { dialogMethods } from '@/utils/methods.js';
+    import { pagination } from '@/utils/pagination.js';
 
     const columns = [
         {
@@ -161,18 +162,10 @@
                 data,
                 columns,
                 selectedRowKeys: [], // Check here to configure the default column
-                loading: false,
-
                 //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
                 scroll: { x: 820, y: 'calc(100vh - 398px)' },
-
                 //  分页信息
-                pagination: {
-                    pageSizeOptions: ['10', '20', '30', '40', '50'],
-                    current: 1,
-                    pageSize: 10,
-                    total: 50,
-                },
+                pagination,
                 //  展示莫泰框
                 dialogVisible: false,
             };
