2ac2848bcbaf99c346f2413a289090d52114891c
xiaoquanquanxin <xxkl123@126.com>
2020-09-30 18:09:24 +0800

    表格

---
+++ src/utils/tableScroll.ts
@@ -0,0 +1,6 @@
+//  表格 设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+
+//	只有一排搜索
+export const oneRowSearch = {x: 820, y: 'calc(100vh - 350px)'};
+//	有2排搜索按钮
+export const towRowSearch = {x: 820, y: 'calc(100vh - 398px)'};
