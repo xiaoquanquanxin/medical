5adbe115312903ce2d2a7763237bbddc8df17cf2
xiaoquanquanxin <xxkl123@126.com>
2020-10-01 22:25:58 +0800

    高度

--- src/utils/routerBasicMap.ts
+++ src/utils/routerBasicMap.ts
@@ -4,7 +4,7 @@ import {RouteConfig} from "vue-router"
 //	根据数据觉定返回那些路由被展示
 export function setRouteListByMap(map: any, asyncRoutesList: any): RouteConfig[] {
 	let list: any[]
-	list = []
+	list = [];
 	asyncRoutesList.forEach((item: any) => {
 		//	如果服务端返回的数据有这个路由
 		if (map[item.name]) {
@@ -14,12 +14,11 @@ export function setRouteListByMap(map: any, asyncRoutesList: any): RouteConfig[]
 				item.children = children;
 			}
 			//	如果是用户列表路由
-			if (item.meta.isUserList) {
+			if (item.meta.isSchemeList) {
 				//	如果没有子路由，直接抛出错误
 				if (!item.children.length) {
 					throw new Error('用户列表必须要有子路由，因为要指定重定向');
 				}
-				item.redirect = item.children[0].path;
 				console.log(item.children);
 				//	放在特殊的列表里
 				store.dispatch('routeList/setUserRouteList', item.children);
