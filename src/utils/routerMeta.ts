4c44960a8443d20cfc796de375651b51439dd43b
xiaoquanquanxin <xxkl123@126.com>
2020-10-01 22:50:34 +0800

    高度

--- src/utils/routerMeta.ts
+++ src/utils/routerMeta.ts
@@ -2,7 +2,13 @@
 import {RouteConfig} from "vue-router"
 
 export function jumpTo(item: RouteConfig) {
-	console.log(item.path);
+	//	console.log(item.path);
+	//	一种特殊情况，对于有页面内的横向子路由的，页面处于子路由，点击了父级路由
+	//	@ts-ignore
+	if ((this.$route.meta.selectRouteKey === item.meta.selectRouteKey) && item.meta.hasTransverseSubPaths) {
+		return;
+	}
+
 	//  自己不跳转自己
 	//	@ts-ignore
 	if (this.$route.path === item.path) {
