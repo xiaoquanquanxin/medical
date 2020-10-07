96b6b0498ca6a8671c7955444c6f7705f1d54c02
xiaoquanquanxin <xxkl123@126.com>
2020-10-07 09:47:43 +0800

    高度

---
+++ src/views/platform/distributors/distributors.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="layout-content-inner-main">
+        <p>渠道商管理</p>
+    </div>
+</template>
+<script>
+    //  渠道商管理
+    export default {
+        components: {},
+        data(){
+            return {};
+        },
+        methods: {}
+    };
+</script>
---
+++ src/views/platform/supplier/supplier.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="layout-content-inner-main">
+        <p>供应商管理</p>
+    </div>
+</template>
+<script>
+    //  供应商管理
+    export default {
+        components: {},
+        data(){
+            return {};
+        },
+        methods: {}
+    };
+</script>
