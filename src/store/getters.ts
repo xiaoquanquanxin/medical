4109bcec173404444e1591635a0fea11125fc14f
xiaoquanquanxin <xxkl123@126.com>
2020-10-07 18:50:02 +0800

    高度

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,8 +1,20 @@
 const getters = {
+	//	侧边栏
 	sidebar: (state: any) => state.app.sidebar,
+	//	路由
 	routeList: (state: any) => state.app.routeList,
+	//	左右框
 	shuttleBox: (state: any) => state.app.shuttleBox,
 	roleBox: (state: any) => state.app.roleBox,
+
+	//	渠道商管理
+	distributors: (state: any) => state.app.distributors,
+	//	家庭组
+	familyGroup: (state: any) => state.app.familyGroup,
+
+
+
 	accountBox: (state: any) => state.app.accountBox,
+	addOral: (state: any) => state.app.addOral,
 };
 export default getters;
