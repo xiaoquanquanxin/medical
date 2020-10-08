2bc7bffdfd7a4e9035fbe7a90da244fb3303a665
xiaoquanquanxin <xxkl123@126.com>
2020-10-08 09:55:27 +0800

    高度

--- src/store/modules/shuttleBox.ts
+++ src/store/modules/shuttleBox.ts
@@ -2,24 +2,34 @@
 import {COMMIT_INTERFACE} from "@/store"
 
 interface STATE {
-	modalTargetKeys: Array<any>
+	modalTargetKeys: Array<any>;
+	modalType: string;
 }
 
-const state = {
-	//	莫泰框的数据
+const state: STATE = {
+	//	穿梭框的类型	🔥🔥🔥 类型非常重要，用于决定拉什么数据
+	modalType: '',
+	//	穿梭框的数据
 	modalTargetKeys: [],
 }
 
 const mutations = {
+	SET_MODAL_TYPE: (state: STATE, modalType: string) => {
+		state.modalType = modalType;
+	},
 	SET_MODAL_TARGET_KEY: (state: STATE, modalTargetKeys: Array<any>) => {
 		state.modalTargetKeys = modalTargetKeys;
-	}
+	},
+
 }
 //	'shuttleBox/setModalTargetKey'
 const actions = {
+	setModalType({commit}: COMMIT_INTERFACE<STATE>, modalType: string) {
+		commit('SET_MODAL_TYPE', modalType)
+	},
 	setModalTargetKey({commit}: COMMIT_INTERFACE<STATE>, modalTargetKeys: Array<any>) {
 		commit('SET_MODAL_TARGET_KEY', modalTargetKeys)
-	}
+	},
 }
 
 
