fde942b7494428ee7670b6c95d48476141dd5ad6
xiaoquanquanxin <xxkl123@126.com>
2020-10-08 17:44:32 +0800

    高度

---
+++ src/components/commodity/addClassificationBox.vue
@@ -0,0 +1,79 @@
+<template>
+    <a-form class="form"
+            :form="form"
+            v-bind="formItemLayout"
+            @submit="handleSubmit"
+            autocomplete="off"
+    >
+        <a-form-item label="仓库名称">
+            <a-input
+                    v-decorator="categoryNameDecorator"
+                    placeholder="请输入分类名称"
+            />
+        </a-form-item>
+        <a-form-item label="分类编码">
+            <p>js</p>
+        </a-form-item>
+        <a-form-item label="是否展示">
+            <a-switch checked-children="开" un-checked-children="关"
+                      v-model="whetherShow"
+            />
+        </a-form-item>
+    </a-form>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        computed: {
+            //  分类id
+            classificationId(){
+                return this.$store.state.commodity.classificationId;
+            },
+        },
+        data(){
+            return {
+                formItemLayout,
+                //  分类名称
+                categoryNameDecorator: ['categoryName', {
+                    rules: [{
+                        required: true,
+                        message: '请输入分类名称'
+                    },]
+                }],
+                //  是否展示
+                whetherShow: false,
+            };
+        },
+        created(){
+            console.log('是编辑？', !!this.classificationId);
+        },
+        methods: {
+            //    表单提交
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
