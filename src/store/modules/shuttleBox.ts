bd71c7599a71aecb6470ab98e00f1ab7a23bdbac
xiaoquanquanxin <xxkl123@126.com>
2020-10-08 10:09:40 +0800

    高度

--- src/store/modules/shuttleBox.ts
+++ src/store/modules/shuttleBox.ts
@@ -1,23 +1,28 @@
 //	穿梭框
 import {COMMIT_INTERFACE} from "@/store"
 
-//	穿梭框数据，用于区分类型
+//	穿梭框区分类型
 export const SHUTTLE_BOX = {
 	//	查看市场价
 	CHECK_MARKET_PRICES: 'CHECK_MARKET_PRICES',
-}
+};
+
 
 interface STATE {
 	modalTargetKeys: Array<any>;
+	shuttleBoxBasicData: any;
 	shuttleBoxType: string;
 }
 
 const state: STATE = {
 	//	穿梭框的类型	🔥🔥🔥 类型非常重要，用于决定拉什么数据
 	shuttleBoxType: '',
+	//	穿梭框的基础数据
+	shuttleBoxBasicData: null,
 	//	穿梭框的数据
 	modalTargetKeys: [],
 }
+
 
 const mutations = {
 	SET_SHUTTLE_BOX_TYPE: (state: STATE, shuttleBoxType: string) => {
