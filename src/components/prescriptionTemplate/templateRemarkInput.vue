c3327f0ab153b98b4135999adfaa492ff082656c
xiaoquanquanxin <xxkl123@126.com>
2020-10-09 20:59:11 +0800

    高度

---
+++ src/components/prescriptionTemplate/templateRemarkInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <a-textarea
+            placeholder="请输入备注"
+            v-model="value"
+            @change="setRemark($event.target.value)"
+            style="min-height: 200px"
+    />
+</template>
+<script>
+    import { mapGetters, mapActions } from 'vuex';
+    //  处方模板管理
+    //  增加口服肠内补充方案 - 备注
+    export default {
+        computed: {
+            remark(){
+                return this.$store.state.prescriptionTemplate.remark;
+            }
+        },
+        data(){
+            return {
+                value: this.remark,
+            };
+        },
+        methods: {
+            ...mapActions('prescriptionTemplate', [
+                //  设置商品列表数据
+                'setRemark',
+            ]),
+        }
+    };
+</script>
