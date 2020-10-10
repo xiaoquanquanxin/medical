f8c63be6e7b277ab9a73ea58f1935e116beb7b8f
xiaoquanquanxin <xxkl123@126.com>
2020-10-10 16:02:18 +0800

    高度

---
+++ src/views/order/order.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="layout-content-inner-main">
+        <!--搜索相关-->
+        <!--搜索相关-->
+        <a-input-group class="a-input-group">
+            <a-row :gutter="8">
+                <a-col :span="4">
+                    <a-input v-model="searchData.orderNumber" placeholder="请输入订单号"/>
+                </a-col>
+                <a-col :span="4">
+                    <a-input v-model="searchData.prescriptionsNumber" placeholder="请输入处方单号"/>
+                </a-col>
+                <a-col :span="4">
+                    <a-input v-model="searchData.patientName" placeholder="请输入患者姓名"/>
+                </a-col>
+                <a-col :span="4">
+                    <a-select
+                            v-model="searchData.status"
+                            style="width:100%;"
+                            placeholder="请选择支付状态"
+                    >
+                        <a-select-option value="1600">1600</a-select-option>
+                        <a-select-option value="160">160</a-select-option>
+                    </a-select>
+                </a-col>
+                <a-col :span="4">
+                    <a-button type="primary">
+                        搜索
+                    </a-button>
+                </a-col>
+            </a-row>
+        </a-input-group>
+        <!--表格-->
+        <a-table
+                :columns="columns"
+                :data-source="data"
+                :scroll="scroll"
+                :pagination="false"
+        >
+            <div slot="operation" slot-scope="scope,sItem,sIndex,extra">
+                <a-space size="small">
+                    <router-link :to="{name:'orderDetail',params:{orderDetailId:'322323'}}">详情</router-link>
+                </a-space>
+            </div>
+        </a-table>
+        <!--分页-->
+        <a-row type="flex" justify="end" class="a-input-group">
+            <a-pagination
+                    v-model="pagination.current"
+                    :page-size-options="pagination.pageSizeOptions"
+                    :total="pagination.total"
+                    show-size-changer
+                    :page-size="pagination.pageSize"
+                    @showSizeChange="onShowSizeChange"
+                    @change="pageChange"
+            >
+                <template slot="buildOptionText" slot-scope="props">
+                    <span>{{ props.value }}条/页</span>
+                </template>
+            </a-pagination>
+        </a-row>
+    </div>
+</template>
+<script>
+    import { pagination } from '@/utils/pagination.ts';
+    import { oneRowSearch } from '../../utils/tableScroll';
+
+    const columns = [
+        {
+            title: '订单号',
+            dataIndex: 'name',
+            width: 100,
+        },
+        {
+            title: '处方单号',
+            dataIndex: 'aaa',
+            width: 100,
+        },
+        {
+            title: '医院名称',
+            dataIndex: 'bbb',
+            width: 100,
+        },
+        {
+            title: '处方医生',
+            dataIndex: 'ccc',
+            width: 100,
+        },
+        {
+            title: '缴费金额',
+            dataIndex: 'coun',
+            width: 100,
+        },
+        {
+            title: '支付状态',
+            dataIndex: 'dddd',
+            width: 100,
+        }, {
+            title: '操作',
+            dataIndex: 'operation',
+            width: 100,
+            scopedSlots: { customRender: 'operation' }
+        },
+    ];
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push({
+            key: i,
+            name: '权鑫',
+            sex: '男',
+            familyGroup: 'xxx',
+        });
+    }
+    //  订单管理
+    export default {
+        data(){
+            return {
+                data,
+                columns,
+                //  搜索数据
+                searchData: {},//  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
+                scroll: oneRowSearch,//  分页信息
+                pagination,
+            };
+        },
+        methods: {
+            //  展示的每一页数据变换
+            onShowSizeChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+                this.pagination.pageSize = pageSize;
+            },
+            //  切换分页页码
+            pageChange(current, pageSize){
+                console.log(current);
+                console.log(pageSize);
+            },
+        }
+    };
+</script>
