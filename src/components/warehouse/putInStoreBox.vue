2653066156d0a5249f008fe69efc3dd3d8a36093
xiaoquanquanxin <xxkl123@126.com>
2020-10-10 19:55:00 +0800

    高度

---
+++ src/components/warehouse/putInStoreBox.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="table-in-box">
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="采购单号">
+                <a-select placeholder="请选择采购单号"
+                          v-decorator="purchaseOrderNoDecorator"
+                >
+                    <a-select-option value="1">
+                        xxx
+                    </a-select-option>
+                    <a-select-option value="2">
+                        xxx
+                    </a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="商品货号">xxxx</a-form-item>
+            <a-form-item label="供应商">xxxx</a-form-item>
+            <a-form-item label="商品品牌">xxxx</a-form-item>
+            <a-form-item label="厂家">xxxx</a-form-item>
+            <a-form-item label="批次">
+                <a-input
+                        v-decorator="batchDecorator"
+                        placeholder="请输入批次"
+                />
+            </a-form-item>
+            <a-form-item label="单位">xxxx</a-form-item>
+            <a-form-item label="生产时间">xxxx</a-form-item>
+            <a-form-item label="过期时间">
+                <a-row style="width:calc((100vw - 200px)*.65)">
+                    <a-col :span="7">
+                        <a-date-picker
+                                @change="onDateChange"
+                        />
+                        <a-input type="hidden" v-decorator="expirationTimeDecorator"/>
+                    </a-col>
+                    <a-col :span="8" :offset="1">
+                        <span>剩余{{2323}}天</span>
+                    </a-col>
+                </a-row>
+            </a-form-item>
+            <a-form-item label="过期剩余天数">
+                <a-row style="width:calc((100vw - 200px)*.65)">
+                    <a-col :span="7">
+                        <a-input
+                                v-decorator="overdueReminderDaysDecorator"
+                                placeholder="请输入过期剩余天数"
+                        />
+                    </a-col>
+                    <a-col :span="8" :offset="1">
+                        <span>天后过期，提醒平台退货</span>
+                    </a-col>
+                </a-row>
+            </a-form-item>
+            <a-form-item label="入库数量">
+                <a-input
+                        v-decorator="putInStoreQuantityDecorator"
+                        placeholder="请输入入库数量"
+                />
+            </a-form-item>
+        </a-form>
+    </div>
+</template>
+<script>
+    import moment from 'moment';
+    import { formItemLayout } from '@/utils/layout.ts';
+    //  入库操作
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        data(){
+            return {
+                formItemLayout,
+                //  采购单号
+                purchaseOrderNoDecorator: ['purchaseOrderNo', {
+                    rules: [{
+                        required: true,
+                        message: '请选择采购单号'
+                    },]
+                }],
+                //  批次
+                batchDecorator: ['batch', {
+                    rules: [{
+                        required: true,
+                        message: '请输入批次'
+                    },]
+                }],
+                //  过期时间
+                expirationTimeDecorator: ['expirationTime', {
+                    rules: [{
+                        required: true,
+                        message: '请选择过期时间'
+                    },]
+                }],
+                //  过期剩余天数
+                overdueReminderDaysDecorator: ['overdueReminder', {
+                    rules: [{
+                        required: true,
+                        message: '请输入过期剩余天数'
+                    },]
+                }],
+                //  入库数量
+                putInStoreQuantityDecorator: ['putInStoreQuantity', {
+                    rules: [{
+                        required: true,
+                        message: '请输入入库数量'
+                    },]
+                }],
+            };
+        },
+        methods: {
+            //  时间选择器的方法
+            moment,
+            //  选择过期时间
+            onDateChange(value, selectDateValue){
+                //  console.log(value.months());
+                this.form.setFieldsValue({
+                    expirationTime: selectDateValue,
+                });
+            },
+            //  表单提交 保存
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        console.log(values.expirationTime);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
