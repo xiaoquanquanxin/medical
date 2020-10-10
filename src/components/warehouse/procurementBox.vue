50ea569fe51f9f9c2d71cfaddaf0669da9fe9bf8
xiaoquanquanxin <xxkl123@126.com>
2020-10-10 18:21:21 +0800

    高度

---
+++ src/components/warehouse/procurementBox.vue
@@ -0,0 +1,105 @@
+<template>
+    <a-form class="form"
+            :form="form"
+            v-bind="formItemLayout"
+            @submit="handleSubmit"
+            autocomplete="off"
+    >
+        <a-form-item label="商家名称">xxxx</a-form-item>
+        <a-form-item label="商品货号">xxxx</a-form-item>
+        <a-form-item label="供应商">xxxx</a-form-item>
+        <a-form-item label="商品品牌">xxxx</a-form-item>
+        <a-form-item label="厂家">xxxx</a-form-item>
+        <a-form-item label="单位">
+            <a-select placeholder="请选择单位"
+                      v-decorator="unitDecorator"
+            >
+                <a-select-option value="1">
+                    xxx
+                </a-select-option>
+                <a-select-option value="2">
+                    xxx
+                </a-select-option>
+            </a-select>
+        </a-form-item>
+        <a-form-item label="采购数量">
+            <a-input
+                    v-decorator="purchaseQuantityDecorator"
+                    placeholder="请输入采购数量"
+            />
+        </a-form-item>
+        <a-form-item label="备注">
+            <a-input
+                    v-decorator="remarkDecorator"
+                    placeholder="请输入备注"
+            />
+        </a-form-item>
+    </a-form>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+    //  采购操作
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        computed: {
+            //  查看的id
+            warehouseId(){
+                return this.$store.state.warehouse.warehouseId;
+            }
+        },
+        data(){
+            return {
+                formItemLayout,
+
+                //  单位
+                unitDecorator: ['unit', {
+                    rules: [{
+                        required: true,
+                        message: '请选择单位'
+                    },]
+                }],
+                //  采购数量
+                purchaseQuantityDecorator: ['purchaseQuantity', {
+                    rules: [{
+                        required: true,
+                        message: '请输入采购数量'
+                    },]
+                }],
+                //  备注
+                remarkDecorator: ['remark', {
+                    rules: [{
+                        required: false,
+                        message: '备注'
+                    },]
+                }],
+
+            };
+        },
+        methods: {
+            //  表单提交 保存
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
---
+++ src/components/warehouse/viewTotalInventory.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="table-in-box">
+        <a-table
+                :columns="columns"
+                :data-source="data"
+                :pagination="false"
+                :scroll="modalTableScroll"
+        >
+        </a-table>
+    </div>
+</template>
+<script>
+    import { modalTableScroll } from '../../utils/tableScroll';
+
+    const columns = [
+        {
+            title: '医院',
+            dataIndex: 'hospital',
+            width: 150,
+        },
+        {
+            title: '总库存',
+            dataIndex: 'totalInventory',
+            width: 150,
+        },
+        {
+            title: '暂用库存',
+            dataIndex: 'abutmentInventory',
+            width: 150,
+        },
+        {
+            title: '剩余库存',
+            dataIndex: 'remainingInventory',
+            width: 150,
+        },
+
+    ];
+    const data = [];
+    for (let i = 0; i < 20; i++) {
+        data.push({
+            key: i,
+            hospital: '经纬医院',
+        });
+    }
+    //  查看总库存
+    export default {
+        computed: {
+            //  查看的id
+            warehouseId(){
+                return this.$store.state.warehouse.warehouseId;
+            }
+        },
+        created(){
+            console.log(this.warehouseId);
+        },
+        data(){
+            return {
+                columns,
+                data,
+                //	弹框table的scroll
+                modalTableScroll,
+
+            };
+        }
+    };
+</script>
