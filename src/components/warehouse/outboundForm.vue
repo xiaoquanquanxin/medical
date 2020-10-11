dff11337a3c677a7cdc276c642f9c604c561f722
xiaoquanquanxin <xxkl123@126.com>
2020-10-11 17:11:54 +0800

    高度

--- src/components/warehouse/outboundForm.vue
+++ src/components/warehouse/outboundForm.vue
@@ -1,36 +1,38 @@
 <template>
-    <a-form class="form"
-            :form="form"
-            v-bind="formItemLayout"
-            @submit="handleSubmit"
-            autocomplete="off"
-    >
-        <a-form-item label="出库商品">
-            <a-select placeholder="请选择出库商品"
-                      v-decorator="outboundGoodsDecorator"
-            >
-                <a-select-option value="1">
-                    xxx
-                </a-select-option>
-                <a-select-option value="2">
-                    xxx
-                </a-select-option>
-            </a-select>
-        </a-form-item>
-        <a-form-item label="商品货号">xxxx</a-form-item>
-        <a-form-item label="供应商">xxxx</a-form-item>
-        <a-form-item label="商品品牌">xxxx</a-form-item>
-        <a-form-item label="厂家">xxxx</a-form-item>
-        <a-form-item label="单位">xxxx</a-form-item>
-        <a-form-item label="生产时间">xxxx</a-form-item>
-        <a-form-item label="过期时间">xxxx</a-form-item>
-        <a-form-item label="出库数量">
-            <a-input
-                    v-decorator="outboundQuantityDecorator"
-                    placeholder="请输入采购数量"
-            />
-        </a-form-item>
-    </a-form>
+    <div class="table-in-box">
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="出库商品">
+                <a-select placeholder="请选择出库商品"
+                          v-decorator="outboundGoodsDecorator"
+                >
+                    <a-select-option value="1">
+                        xxx
+                    </a-select-option>
+                    <a-select-option value="2">
+                        xxx
+                    </a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="商品货号">xxxx</a-form-item>
+            <a-form-item label="供应商">xxxx</a-form-item>
+            <a-form-item label="商品品牌">xxxx</a-form-item>
+            <a-form-item label="厂家">xxxx</a-form-item>
+            <a-form-item label="单位">xxxx</a-form-item>
+            <a-form-item label="生产时间">xxxx</a-form-item>
+            <a-form-item label="过期时间">xxxx</a-form-item>
+            <a-form-item label="出库数量">
+                <a-input
+                        v-decorator="outboundQuantityDecorator"
+                        placeholder="请输入采购数量"
+                />
+            </a-form-item>
+        </a-form>
+    </div>
 </template>
 <script>
     import { formItemLayout } from '@/utils/layout.ts';
