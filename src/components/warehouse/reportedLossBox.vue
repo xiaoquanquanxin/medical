22ae5631d33a0f3fac28a028ed8d6dcc743c0303
xiaoquanquanxin <xxkl123@126.com>
2020-10-11 16:29:25 +0800

    高度

---
+++ src/components/warehouse/reportedLossBox.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="table-in-box">
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="报损商品">
+                <a-select placeholder="请选择报损商品"
+                          v-decorator="reportedLossGoodsDecorator"
+                >
+                    <a-select-option value="1">
+                        xxx
+                    </a-select-option>
+                    <a-select-option value="2">
+                        xxx
+                    </a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item label="商品货号">xxxx</a-form-item>
+            <a-form-item label="供应商">xxxx</a-form-item>
+            <a-form-item label="商品品牌">xxxx</a-form-item>
+            <a-form-item label="厂家">xxxx</a-form-item>
+            <a-form-item label="单位">xxxx</a-form-item>
+            <a-form-item label="生产时间">xxxx</a-form-item>
+            <a-form-item label="报损数量">
+                <a-input
+                        v-decorator="reportedLossQuantityDecorator"
+                        placeholder="请输入采购数量"
+                />
+            </a-form-item>
+            <a-form-item label="报损原因">
+                <div style="width:calc((100vw - 200px)*.65)">
+                    <a-textarea
+                            :auto-size="{ minRows: 3, maxRows: 5 }"
+                            v-decorator="reportedLossReasonDecorator"
+                            placeholder="请输入报损原因"
+                    />
+                </div>
+            </a-form-item>
+        </a-form>
+    </div>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+    //  报损操作
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        data(){
+            return {
+                formItemLayout,
+                //  报损商品
+                reportedLossGoodsDecorator: ['reportedLossGoods', {
+                    rules: [{
+                        required: true,
+                        message: '请选择报损商品'
+                    },]
+                }],
+                //  报损数量
+                reportedLossQuantityDecorator: ['reportedLossQuantity', {
+                    rules: [{
+                        required: true,
+                        message: '请输入采购数量'
+                    },]
+                }],
+                //  报损原因
+                reportedLossReasonDecorator: ['reportedLossReason', {
+                    rules: [{
+                        required: true,
+                        message: '请输入报损原因'
+                    },]
+                }],
+            };
+        },
+        methods: {
+            //  表单提交 保存
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
