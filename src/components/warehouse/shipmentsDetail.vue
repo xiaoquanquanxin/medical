51fe80e7ab578bc4ee4cdd04355d2385e0f57f82
xiaoquanquanxin <xxkl123@126.com>
2020-10-11 17:22:56 +0800

    高度

---
+++ src/components/warehouse/shipmentsDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="table-in-box">
+        <a-form class="form"
+                autocomplete="off"
+                v-bind="formItemLayout"
+        >
+            <a-form-item label="发货数量">323223</a-form-item>
+            <a-form-item label="选择物流公司">323223</a-form-item>
+            <a-form-item label="生成快递单号">323223</a-form-item>
+            <a-form-item label="备注">323223</a-form-item>
+            <a-form-item label="报损">323223</a-form-item>
+        </a-form>
+    </div>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+
+    export default {
+        computed: {
+            //  发货id
+            procurementId(){
+                return this.$store.state.warehouse.procurementId;
+            }
+        },
+        data(){
+            return {
+                formItemLayout,
+            };
+        },
+        created(){
+            console.log(this.procurementId);
+        },
+    };
+</script>
