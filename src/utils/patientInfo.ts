f7af525a2ad5f803838c34f38c7dfc9e7f2893be
xiaoquanquanxin <xxkl123@126.com>
2020-10-13 11:26:22 +0800

    高度

---
+++ src/utils/patientInfo.ts
@@ -0,0 +1,20 @@
+//	病人信息、直接编辑用的 描述框的方法
+export const descriptionsMethods = {
+	//	聚焦事件
+	descriptionFormFocusFn(activeElementId: number) {
+		//	@ts-ignore
+		this.activeElementId = activeElementId;
+		console.log(activeElementId);
+	},
+	//	点击事件
+	descriptionFormClickFn(activeElementId: number, event: any) {
+		//	@ts-ignore
+		this.activeElementId = activeElementId;
+		const td = event.target.parentNode;
+		//	@ts-ignore
+		this.$nextTick(() => {
+			const el = td.querySelector('.form-element');
+			el.focus();
+		});
+	},
+}
