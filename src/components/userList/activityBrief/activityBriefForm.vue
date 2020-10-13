ef29cef65bf1c7f3eba22d0d6ebc22825870769f
xiaoquanquanxin <xxkl123@126.com>
2020-10-13 21:18:57 +0800

    高度

---
+++ src/components/userList/activityBrief/activityBriefForm.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="table-in-box">
+        <a-form class="form"
+                :form="form"
+                v-bind="formItemLayout"
+                @submit="handleSubmit"
+                autocomplete="off"
+        >
+            <a-form-item label="内容">
+                <div style="width:500px;">
+                    <a-textarea
+                            :auto-size="{ minRows: 3, maxRows: 5 }"
+                            v-decorator="contentDecorator"
+                            placeholder="请输入活动小结"
+                    />
+                </div>
+            </a-form-item>
+        </a-form>
+    </div>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+    //  活动小结操作
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        computed: {
+            progressNoteId(){
+                return this.$store.state.userList.progressNoteId;
+            }
+        },
+        data(){
+            return {
+                formItemLayout,
+                //  内容
+                contentDecorator: ['content', {
+                    rules: [{
+                        required: true,
+                        message: '请输入活动小结'
+                    },]
+                }],
+            };
+        },
+        created(){
+            console.log('是编辑？', !!this.progressNoteId);
+        },
+        methods: {
+            //  表单提交 保存
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
