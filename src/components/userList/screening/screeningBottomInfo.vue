341b555625f3eb2aee5eef8016d192e408f1f4d7
xiaoquanquanxin <xxkl123@126.com>
2020-10-13 16:45:23 +0800

    高度

--- src/components/userList/screening/screeningBottomInfo.vue
+++ src/components/userList/screening/screeningBottomInfo.vue
@@ -1,82 +1,73 @@
 <template>
-    <a-table
-            :columns="screenBottomColumns"
-            :data-source="screenBottomData"
-            :pagination="false"
-            :showHeader="false"
-            bordered
-    >
-        <div slot="screeningPeople" slot-scope="scope,sItem,sIndex,extra">
-            <a-input placeholder="请输入筛查人"/>
-        </div>
-        <div slot="date" slot-scope="scope,sItem,sIndex,extra">
-            <a-date-picker
-                    addon-before="选择日期："
-                    placeholder="请选择日期"
-                    @change="onDateChange"
-            />
-        </div>
-        <div slot="time" slot-scope="scope,sItem,sIndex,extra">
-            <a-time-picker addon-before="选择时间："
-                           v-model="selectTimeMoment"
-                           @change="selectTimeChange"
-                           format="HH:mm"/>
-        </div>
-    </a-table>
+    <div class="item-3">
+        <a-descriptions
+                :title="null"
+                bordered
+                :column="3"
+                size="small"
+        >
+            <a-descriptions-item label="筛查人">
+                <a-input placeholder="请输入筛查人"/>
+            </a-descriptions-item>
+            <!--选择日期-->
+            <a-descriptions-item label="选择日期">
+                <a-date-picker
+                        placeholder="请选择日期"
+                        v-model="selectDateMoment"
+                        @change="onDateChange"
+                />
+            </a-descriptions-item>
+            <a-descriptions-item label="选择时间">
+                <a-time-picker
+                        placeholder="请选择时间"
+                        v-model="selectTimeMoment"
+                        @change="selectTimeChange"
+                        format="HH:mm"/>
+            </a-descriptions-item>
+        </a-descriptions>
+    </div>
 </template>
 <script>
-    //  底部信息
-    const screenBottomColumns = [
-        {
-            dataIndex: 'a',
-        },
-        {
-            dataIndex: 'b',
-            scopedSlots: { customRender: 'screeningPeople' },
-        },
-        {
-            dataIndex: 'c',
-        },
-        {
-            dataIndex: 'd',
-            scopedSlots: { customRender: 'date' }
-        },
-        {
-            dataIndex: 'e',
-        },
-        {
-            dataIndex: 'f',
-            scopedSlots: { customRender: 'time' }
-        },
-    ];
-
     //  底部信息
     export default {
         computed: {
             screenBottomData(){
+                console.log(this.$store.state.userList.screeningInfo);
                 return this.$store.state.userList.screeningInfo.screenBottomData;
             }
         },
         data(){
             return {
+                //  选择日期的值的对象
+                selectDateMoment: null,
+                //  选择日期的值
+                selectDateValue: null,
                 //  选择时间的值的对象
                 selectTimeMoment: null,
                 //  选择时间的值
                 selectTimeValue: null,
-                //  底部信息
-                screenBottomColumns,
             };
         },
+
+        watch: {
+            screenBottomData(value){
+                console.log(value)
+            }
+        },
         created(){
-            console.table(JSON.parse(JSON.stringify(this.screenBottomData[0])));
+//            setTimeout(() => {
+//                console.table(JSON.parse(JSON.stringify(this.screenBottomData)));
+//            }, 332);
         },
         methods: {
             //  选择过期日期
             onDateChange(value, selectDateValue){
                 console.log(selectDateValue);
+
             },
             //  选择时间的变换
             selectTimeChange(value, selectTimeValue){
+                console.log(selectTimeValue);
                 this.selectTimeValue = selectTimeValue;
             },
         }
