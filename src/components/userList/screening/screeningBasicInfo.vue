0c34221edef39bd1cf4114a661f8ca0c23ee5e91
xiaoquanquanxin <xxkl123@126.com>
2020-10-13 15:00:51 +0800

    高度

---
+++ src/components/userList/screening/screeningBasicInfo.vue
@@ -0,0 +1,47 @@
+<template>
+    <a-table
+            :columns="columns"
+            :data-source="screeningBasicInfo"
+            :pagination="false"
+            :showHeader="false"
+            bordered
+    >
+    </a-table>
+</template>
+<script>
+    //  基础信息
+    const columns = [
+        {
+            dataIndex: 'a',
+        },
+        {
+            dataIndex: 'b',
+        },
+        {
+            dataIndex: 'c',
+        },
+        {
+            dataIndex: 'd',
+        },
+        {
+            dataIndex: 'e',
+        },
+        {
+            dataIndex: 'f',
+        },
+    ];
+
+    export default {
+        computed: {
+            screeningBasicInfo(){
+                return this.$store.state.userList.screeningInfo.screeningBasicInfo;
+            }
+        },
+        data(){
+            return {
+                columns,
+
+            };
+        }
+    };
+</script>
