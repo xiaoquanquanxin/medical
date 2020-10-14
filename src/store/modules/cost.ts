3fcc989767c0927a491d2bebfc461f133b930422
xiaoquanquanxin <xxkl123@126.com>
2020-10-14 18:51:35 +0800

    高度

--- src/store/modules/cost.ts
+++ src/store/modules/cost.ts
@@ -4,21 +4,30 @@ import {COMMIT_INTERFACE} from "@/store";
 interface STATE {
 	//	收计费被选中的id
 	selectCostId: number;
+	//	操作类型	缴费1 、退费-1
+	costType: 1 | -1;
 }
 
 //	有id，就是编辑了
 const state: STATE = {
 	selectCostId: 0,
+	costType: 1,
 }
 const mutations = {
-	SET_SELECT_COST_ID: (state: STATE, selectCostId: any) => {
+	SET_SELECT_COST_ID: (state: STATE, selectCostId: number) => {
 		state.selectCostId = selectCostId;
+	},
+	SET_COST_TYPE: (state: STATE, costType: 1 | -1) => {
+		state.costType = costType;
 	},
 }
 
 const actions = {
-	setSelectCostId: ({commit}: COMMIT_INTERFACE<STATE>, selectCostId: any) => {
+	setSelectCostId: ({commit}: COMMIT_INTERFACE<STATE>, selectCostId: number) => {
 		commit('SET_SELECT_COST_ID', selectCostId);
+	},
+	setCostType: ({commit}: COMMIT_INTERFACE<STATE>, costType: 1 | -1) => {
+		commit('SET_COST_TYPE', costType);
 	},
 }
 
