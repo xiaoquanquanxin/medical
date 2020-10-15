5509edb89fe3fd7473133b1c789b1b182778a949
xiaoquanquanxin <xxkl123@126.com>
2020-10-15 18:54:02 +0800

    高度

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -8,10 +8,22 @@ export function compareToFirstPassword(passwordKey: string) {
 	return (rule: any, value: string, callback: Function) => {
 		//	@ts-ignore
 		const form = this.form;
-		if (value && value !== form.getFieldValue(passwordKey)) {
-			callback('确认密码不对');
+		if (!value || value !== form.getFieldValue(passwordKey)) {
+			callback('确认密码不符');
 		} else {
 			callback();
 		}
+	}
+}
+
+/**
+ * 验证手机号
+ * */
+export function isPhoneNumber(rule: any, value: string, callback: Function) {
+	const reg = /^1[3456789]\d{9}$/;
+	if (!value || !reg.test(value)) {
+		callback('请输入正确手机号');
+	} else {
+		callback();
 	}
 }
