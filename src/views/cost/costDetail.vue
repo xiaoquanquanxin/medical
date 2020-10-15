76c5dc84322054992ab6135c805c65c87f272e1f
xiaoquanquanxin <xxkl123@126.com>
2020-10-15 11:09:46 +0800

    高度

--- src/views/cost/costDetail.vue
+++ src/views/cost/costDetail.vue
@@ -2,5 +2,16 @@
     <div>收计费详情</div>
 </template>
 <script>
-    export default {};
+    export default {
+        data(){
+            return {
+                returnPremiumDetailId: this.$route.params.returnPremiumDetailId,
+                paymentRecordsDetailId: this.$route.params.paymentRecordsDetailId,
+            };
+        },
+        created(){
+            console.log('是退费', this.returnPremiumDetailId);
+            console.log('是缴费', this.paymentRecordsDetailId);
+        }
+    };
 </script>
