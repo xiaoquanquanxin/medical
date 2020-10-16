b9bd0e641e7460f15e032e5ce89edc2c77d82904
xiaoquanquanxin <xxkl123@126.com>
2020-10-16 17:15:29 +0800

    高度

---
+++ src/components/detialsComplexTable/oralLikeComplexTable.vue
@@ -0,0 +1,77 @@
+<template>
+    <a-table
+            :columns="columns"
+            :data-source="dataSource"
+            :pagination="false"
+            bordered
+    >
+        <!--商品名称-->
+        <div slot="commodityName" slot-scope="scope,sItem,sIndex,extra">
+            <div class="negative-margin-16">
+                <div v-for="item in scope.childrenList"
+                     class="negative-margin-item">
+                    {{item.commodity}}
+                </div>
+            </div>
+        </div>
+        <!--配置量-->
+        <div slot="quality" slot-scope="scope,sItem,sIndex,extra">
+            <div class="negative-margin-16">
+                <div v-for="item in scope.childrenList"
+                     class="negative-margin-item">
+                    {{item.quality}}
+                </div>
+            </div>
+        </div>
+    </a-table>
+</template>
+<script>
+    export default {
+        props: ['data-source',],
+        data(){
+            const columns = [
+                {
+                    title: '时间',
+                    dataIndex: 'time',
+                    width: 100,
+                },
+                {
+                    title: '商品名称',
+                    width: 100,
+                    scopedSlots: { customRender: 'commodityName' },
+                },
+                {
+                    title: '配置量',
+                    width: 100,
+                    scopedSlots: { customRender: 'quality' },
+                },
+                {
+                    title: '温水',
+                    dataIndex: 'water',
+                    width: 100,
+                },
+                {
+                    title: '备注',
+                    width: 100,
+                    customRender: (text, row, index) => {
+                        const obj = {
+                            children: text.remark,
+                            attrs: {},
+                        };
+                        if (index === 0) {
+                            obj.attrs.rowSpan = 1000;
+                        } else {
+                            obj.attrs.rowSpan = 0;
+                        }
+                        return obj;
+                    },
+                },
+            ];
+            return {
+                columns,
+            };
+        }
+    };
+</script>
+<style scoped>
+</style>
