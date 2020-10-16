abbed1b20700b4fc75b9e63b1bde5b7dafcc199a
xiaoquanquanxin <xxkl123@126.com>
2020-10-16 15:08:21 +0800

    高度

---
+++ src/components/auditList/rejectForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <a-form class="form"
+            :form="form"
+            v-bind="formItemLayout"
+            @submit="handleSubmit"
+            autocomplete="off"
+    >
+        <a-form-item label="驳回原因">
+            <div style="width:calc((100vw - 200px)*.65)">
+                <a-textarea
+                        placeholder="请输入驳回原因"
+                        v-decorator="rejectDecorator"
+                        :auto-size="{ minRows: 3, maxRows: 5 }"
+                />
+            </div>
+        </a-form-item>
+    </a-form>
+</template>
+<script>
+    import { formItemLayout } from '@/utils/layout.ts';
+    //  驳回原因
+    export default {
+        beforeCreate(){
+            this.form = this.$form.createForm(this);
+        },
+        data(){
+            return {
+                //  详情的id
+                auditDetailId: this.$route.params.auditDetailId,
+                formItemLayout,
+
+                //  驳回原因
+                rejectDecorator: ['reject', {
+                    rules: [{
+                        required: true,
+                        message: '请输入驳回原因',
+                    },]
+                }]
+            };
+        },
+        methods: {
+            //  表单提交 保存
+            handleSubmit(){
+                return new Promise(((resolve, reject) => {
+                    console.log(this.form);
+                    this.form.validateFields((err, values) => {
+                        console.table(values);
+                        if (!err) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                }))
+                    .then(v => {
+                        return new Promise(((resolve, reject) => {
+                            console.log('发请求吧');
+                            setTimeout(() => {
+                                resolve();
+                            }, 1000);
+                        }));
+                    });
+            },
+        }
+    };
+</script>
