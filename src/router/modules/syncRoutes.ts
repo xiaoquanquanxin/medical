7ed0aa91f17b1eaf38f4961901af05a4f92ba8bd
xiaoquanquanxin <xxkl123@126.com>
2020-10-17 10:46:22 +0800

    高度

--- src/router/modules/syncRoutes.ts
+++ src/router/modules/syncRoutes.ts
@@ -4,17 +4,21 @@ import Login from "@/views/Login.vue";
 //	同步路由
 export const syncRoutesList: Array<any> = [
 	{
+		meta: {
+			hidden: true,
+		},
 		path: '/404',
 		name: '404',
 		component: Page404,
-		hidden: true
 	},
 	{
-		chName: '登录',
+		meta: {
+			hidden: true,
+			chName: '登录',
+		},
 		path: '/login',
 		name: 'login',
 		component: Login,
-		hidden: true
 	},
 ];
 
