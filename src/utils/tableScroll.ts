a271e06ec07b54b3c69cc7aef89a8daba16dcd78
xiaoquanquanxin <xxkl123@126.com>
2020-10-18 20:49:21 +0800

    高度

--- src/utils/tableScroll.ts
+++ src/utils/tableScroll.ts
@@ -1,48 +1,22 @@
 //  表格 设置横向或纵向滚动，也可用于指定滚动区域的宽和高
 
 //	只有一排搜索
-export const oneRowSearch = (columns: [{ width: number }], y: string) => {
-	return {
-		y: 'calc(100vh - 315px)',
-		x: columns.reduce((a, b) => {
-			return a + b.width;
-		}, 0),
-	}
-}
+export const oneRowSearch = fn('calc(100vh - 315px)');
 //	有2排搜索按钮
-export const twoRowSearch = (columns: [{ width: number }], y: string) => {
-	return {
-		y: 'calc(100vh - 357px)',
-		x: columns.reduce((a, b) => {
-			return a + b.width;
-		}, 0),
-	}
-}
+export const twoRowSearch = fn('calc(100vh - 357px)')
 //	有3排搜索按钮
-export const threeRowSearch = {x: 820, y: 'calc(100vh - 405px)'};
-
+export const threeRowSearch = fn('calc(100vh - 405px)');
 
 //	弹框table的scroll
 export const modalTableScroll = {x: 750, y: 400};
 
-
-//  表格 设置横向或纵向滚动，也可用于指定滚动区域的宽和高
-//
-// //	只有一排搜索
-// export const oneRowSearch = (columns: [{ width: number }]) => {
-// 	return fn(columns, 'calc(100vh - 300px)')
-// }
-//
-// //	有2排搜索按钮
-// export const twoRowSearch = (columns: [{ width: number }]) => {
-// 	return fn(columns, 'calc(100vh - 340px)')
-// }
-
-const fn = (columns: [{ width: number }], y: string) => {
-	return {
-		x: columns.reduce((a, b) => {
-			return a + b.width;
-		}, 0),
-		y,
+function fn(y: string) {
+	return (columns: [{ width: number }]) => {
+		return {
+			x: columns.reduce((a, b) => {
+				return a + b.width;
+			}, 0),
+			y,
+		}
 	}
-};
+}
