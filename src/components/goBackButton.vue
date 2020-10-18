445c3eefa714d4cac215c2b93f5cc2ede3ac6684
xiaoquanquanxin <xxkl123@126.com>
2020-10-18 18:48:19 +0800

    高度

---
+++ src/components/goBackButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <a-button type="primary" @click="back">返回</a-button>
+</template>
+<script>
+    //  返回按钮
+    export default {
+        methods: {
+            back(){
+                this.$router.back();
+            }
+        }
+    };
+</script>
