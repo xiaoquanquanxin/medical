df3486446f86f781f61161d92e6503af8f650620
xiaoquanquanxin <xxkl123@126.com>
2020-10-19 16:01:30 +0800

    高度

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <div id="app">
-        <MyLayout/>
+        <a-config-provider :locale="zh_CN">
+            <MyLayout/>
+        </a-config-provider>
     </div>
 </template>
 <script lang="ts">
 	import {Vue, Component} from 'vue-property-decorator';
 	import MyLayout from '@/layout/layout.vue';
+	//  @ts-ignore
+	import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
 
 	@Component({
 		components: {
@@ -13,7 +17,12 @@
 		}
 	})
 	export default class App extends Vue {
+		zh_CN: any;
 
+		constructor() {
+			super();
+			this.zh_CN = zh_CN;
+		}
 	}
 </script>
 <style lang="css">
