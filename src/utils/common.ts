85f30b7b86a6b5dc523f4f53f1236c759dd8d345
xiaoquanquanxin <xxkl123@126.com>
2020-10-19 17:53:19 +0800

    高度

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,3 +2,20 @@
 export function preventDefault(e: Event) {
 	e.preventDefault();
 }
+
+export function getStorage(key: string) {
+	return localStorage.getItem(key);
+}
+
+export function setStorage(key: string, value: any) {
+	console.log('设置token：', key, value);
+	return localStorage.setItem(key, value);
+}
+
+export function removeStorage(key: string) {
+	localStorage.removeItem(key);
+}
+
+export function clearStorage(key: string) {
+	localStorage.clear();
+}
