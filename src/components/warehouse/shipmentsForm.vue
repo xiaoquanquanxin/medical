92c35b9bf38f50a95348466656d2725a4e051cb0
xiaoquanquanxin <xxkl123@126.com>
2020-10-21 16:12:45 +0800

    高度

--- src/components/warehouse/shipmentsForm.vue
+++ src/components/warehouse/shipmentsForm.vue
@@ -107,7 +107,6 @@
             //  表单提交 保存
             handleSubmit(){
                 return new Promise(((resolve, reject) => {
-                    console.log(this.form);
                     this.form.validateFields((err, values) => {
                         console.table(values);
                         console.log(values.expirationTime);
