6447cf60c43e45ba6e80e22182011415ac66aef8
xiaoquanquanxin <xxkl123@126.com>
2020-10-22 22:16:06 +0800

    高度

--- src/components/userList/progressNote/progressNoteForm.vue
+++ src/components/userList/progressNote/progressNoteForm.vue
@@ -7,15 +7,15 @@
                 autocomplete="off"
         >
             <a-form-item label="是否能够按照方案执行">
-                <a-radio-group v-decorator="performDecorator">
+                <a-radio-group v-decorator="executionDecorator">
                     <a-radio :value="1">是</a-radio>
                     <a-radio :value="2">否</a-radio>
                 </a-radio-group>
             </a-form-item>
             <a-form-item label="具体原因">
-                <a-input
-                        v-decorator="performCauseDecorator"
-                        placeholder="请输入具体原因"/>
+                <a-input class="add-form-input"
+                         v-decorator="executionContentDecorator"
+                         placeholder="请输入具体原因"/>
             </a-form-item>
             <a-form-item label="是否能够耐受">
                 <a-radio-group v-decorator="toleranceDecorator">
@@ -24,9 +24,9 @@
                 </a-radio-group>
             </a-form-item>
             <a-form-item label="具体原因">
-                <a-input
-                        v-decorator="toleranceCauseDecorator"
-                        placeholder="请输入具体原因"/>
+                <a-input class="add-form-input"
+                         v-decorator="toleranceContentDecorator"
+                         placeholder="请输入具体原因"/>
             </a-form-item>
             <a-form-item label="有无大便">
                 <a-radio-group v-decorator="shitDecorator">
@@ -35,9 +35,9 @@
                 </a-radio-group>
             </a-form-item>
             <a-form-item label="具体原因">
-                <a-input
-                        v-decorator="shitCauseDecorator"
-                        placeholder="请输入具体原因"/>
+                <a-input class="add-form-input"
+                         v-decorator="shitContentDecorator"
+                         placeholder="请输入具体原因"/>
             </a-form-item>
             <a-form-item label="有无饥饿感">
                 <a-radio-group v-decorator="hungerDecorator">
@@ -46,21 +46,26 @@
                 </a-radio-group>
             </a-form-item>
             <a-form-item label="具体原因">
-                <a-input
-                        v-decorator="hungerCauseDecorator"
-                        placeholder="请输入具体原因"/>
+                <a-input class="add-form-input"
+                         v-decorator="hungerContentDecorator"
+                         placeholder="请输入具体原因"/>
             </a-form-item>
         </a-form>
     </div>
 </template>
 <script>
     import { formItemLayout } from '@/utils/layout.ts';
+    import { requestDiseaseRecordSave } from '../../../api/userList/progressNote';
     //  病程记录操作
     export default {
         beforeCreate(){
             this.form = this.$form.createForm(this);
         },
         computed: {
+            //  页面参数 - 病人id
+            patientId(){
+                return this.$route.params.patientId;
+            },
             progressNoteId(){
                 return this.$store.state.userList.progressNoteId;
             }
@@ -69,10 +74,10 @@
             return {
                 formItemLayout,
                 //  是否能够按照方案执行
-                performDecorator: ['perform', {
+                executionDecorator: ['execution', {
                     initialValue: 1,
                 }],
-                performCauseDecorator: ['performCause', {
+                executionContentDecorator: ['executionContent', {
                     rules: [{
                         required: true,
                         message: '请输入具体原因'
@@ -83,7 +88,7 @@
                 toleranceDecorator: ['tolerance', {
                     initialValue: 1,
                 }],
-                toleranceCauseDecorator: ['toleranceCause', {
+                toleranceContentDecorator: ['toleranceContent', {
                     rules: [{
                         required: true,
                         message: '请输入具体原因'
@@ -94,7 +99,7 @@
                 shitDecorator: ['shit', {
                     initialValue: 1,
                 }],
-                shitCauseDecorator: ['shitCause', {
+                shitContentDecorator: ['shitContent', {
                     rules: [{
                         required: true,
                         message: '请输入具体原因'
@@ -105,7 +110,7 @@
                 hungerDecorator: ['hunger', {
                     initialValue: 1,
                 }],
-                hungerCauseDecorator: ['hungerCause', {
+                hungerContentDecorator: ['hungerContent', {
                     rules: [{
                         required: true,
                         message: '请输入具体原因'
@@ -115,8 +120,6 @@
         },
         created(){
             console.log('是编辑？', !!this.progressNoteId);
-        },
-        created(){
             this.searchFn();
         },
         methods: {
@@ -140,21 +143,42 @@
                     this.form.validateFields((err, values) => {
                         console.table(values);
                         if (!err) {
-                            resolve();
+                            resolve(values);
                         } else {
                             reject();
                         }
                     });
                 }))
-                    .then(v => {
-                        return new Promise(((resolve, reject) => {
-                            console.log('发请求吧');
-                            setTimeout(() => {
-                                resolve();
-                            }, 1000);
-                        }));
+                    .then(values => {
+                        return new Promise((resolve, reject) => {
+                            requestDiseaseRecordSave(Object.assign({
+                                    dataType: 2,
+                                    doctorId: 2,
+                                    patientId: this.patientId,
+                                    ...values,
+                                },
+                            ))
+                                .then(v => {
+                                    resolve();
+                                })
+                                .catch(err => {
+                                    reject();
+                                });
+                        });
                     });
             },
         }
     };
+    //	dataType	⚠️todo  数据类型(1内网，2外网)
+    //	doctorId	⚠️️todo  录入医生id
+    //	patientId	病人id
+
+    //	execution	是否能够按照方案执行
+    //	executionContent	说明原因
+    //	hunger	是否有饥饿感
+    //	hungerContent	是否有饥饿感备注
+    //	shit	有无大便
+    //	shitContent	说明性状、次数
+    //	tolerance	是否能够耐受
+    //	toleranceContent	不耐受症状
 </script>
