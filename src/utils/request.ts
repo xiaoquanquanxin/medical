90db43313899dedc8703bf37c966cab931eac6b0
xiaoquanquanxin <xxkl123@126.com>
2020-10-22 21:16:35 +0800

    高度

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -10,26 +10,28 @@ const request = axios.create({
 	//	当跨域请求时发送cookie
 	// withCredentials: true,
 	timeout: 5000,
-	transformRequest: [(searchParams => {
-		console.log(searchParams);
-		if (!searchParams) {
-			return searchParams;
-		}
-		let exportUrl = ''
-		Object.keys(searchParams).map((key) => {
-			exportUrl += key + '=' + searchParams[key] + '&';
-		});
-		exportUrl = exportUrl.slice(0, -1)
-		console.log(exportUrl);
-		return exportUrl;
-	})]
+	// transformRequest: [(searchParams => {
+	// 	return searchParams;
+	// 	console.log(searchParams);
+	// 	if (!searchParams) {
+	// 		return searchParams;
+	// 	}
+	// 	let exportUrl = ''
+	// 	Object.keys(searchParams).map((key) => {
+	// 		exportUrl += key + '=' + searchParams[key] + '&';
+	// 	});
+	// 	exportUrl = exportUrl.slice(0, -1)
+	// 	console.log(exportUrl);
+	// 	return exportUrl;
+	// })]
 });
 
 //	拦截器
 request.interceptors.request.use(
 	config => {
-		config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-		config.headers['Authorization'] = '1';
+		//	config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+		config.headers.post['Content-Type'] = 'application/json';
+		//	config.headers['Authorization'] = '1';
 		return config;
 	},
 	error => {
