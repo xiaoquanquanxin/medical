fd68b54a1fb89291d1f4319983eb46de0b894093
xiaoquanquanxin <xxkl123@126.com>
2020-10-23 16:47:46 +0800

    高度

--- src/api/userList/userList.ts
+++ src/api/userList/userList.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request';
 
-//	病人列表分页		✅，年龄我计算？不行，要输入的是年龄不是生日； BML，是L，不是I
+//	病人列表分页		✅
 export function requestPatientPage(data: any) {
 	return request({
 		url: '/api/patient/page',
@@ -9,7 +9,7 @@ export function requestPatientPage(data: any) {
 	});
 }
 
-//	病人新增			❌404
+//	病人新增			✅
 export function requestPatientSave(data: any) {
 	return request({
 		url: '/api/patient/save',
@@ -18,15 +18,17 @@ export function requestPatientSave(data: any) {
 	});
 }
 
-//	查询病人详细信息	✅
+//	查询病人详细信息	✅，❌查询到的参数和原型不对应，这回导致修改病人失败，同样的，新增病人接口也是一样的参数，所以需要重新确定
+//						对于查询：年龄不是生日，就诊科室没有数据，营养师没数据，病人状态；很多地方我看用的是字典的id?
+//						对于保存：还有啥叫病区和劳动强度，虽然不影响保存接口，还有为什么得删除就诊病号才能成功[jzbh]，
 export function requestPatientSelectOnePatient(patientId: string | number) {
 	return request({
 		url: `/api/patient/selectOnePatient/${patientId}`,
-		method: 'post',
+		method: 'get',
 	});
 }
 
-//	病人修改			❌404
+//	病人修改			❌，同上
 export function requestPatientUpdate(data: any) {
 	return request({
 		url: '/api/patient/update',
