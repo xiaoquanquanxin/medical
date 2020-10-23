b53405df3183119067877715a063aecacee128fc
xiaoquanquanxin <xxkl123@126.com>
2020-10-23 20:54:29 +0800

    高度

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -14,3 +14,13 @@ export function uploadHandleChange({file}: { file: any }, key: string, thumbUrl:
 		this[thumbUrl] = response.thumbUrl;
 	}
 }
+
+
+//  上传之前
+export function beforeUploadFn(file: any) {
+	beforeUploadData.type = file.type;
+	beforeUploadData.name = file.name;
+}
+
+//	上传文件的数据，这样的对象只需要一个
+export const beforeUploadData: { type: string, name: string } = {type: '', name: ''}
