0377a2d5a7ef82168451e8e442420b33931f727f
xiaoquanquanxin <xxkl123@126.com>
2020-10-24 10:37:17 +0800

    高度

---
+++ src/api/questionnaire.ts
@@ -0,0 +1,45 @@
+import request from '@/utils/request';
+
+//  评估分页列表			❌，几天前能用，现在404
+export function requestAssessPage(data: any) {
+	return request({
+		url: '/api/assess/page',
+		method: 'post',
+		data,
+	});
+}
+
+//	新增评估信息			❌，同上
+export function requestAssessInsert(data: any) {
+	return request({
+		url: '/api/assess/insert',
+		method: 'post',
+		data,
+	})
+}
+
+//	获取评估信息			⚠️，几天前能用
+export function requestAssessGet(id: string | number) {
+	return request({
+		url: `/api/assess/get/${id}`,
+		method: 'get',
+	})
+}
+
+//	修改评估信息			⚠️，几天前能用
+export function requestAssessUpdate(data: any) {
+	return request({
+		url: '/api/assess/update',
+		method: 'post',
+		data,
+	})
+}
+
+
+//	删除评估信息			⚠️原型无
+export function requestAssessDelete(id: string | number) {
+	return request({
+		url: `/api/assess/delete/${id}`,
+		method: 'get',
+	})
+}
