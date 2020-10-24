8e5acb388f9f8be98e168cf6304c24416978bf26
xiaoquanquanxin <xxkl123@126.com>
2020-10-24 17:46:29 +0800

    高度

--- src/views/auditList/auditList.vue
+++ src/views/auditList/auditList.vue
@@ -110,28 +110,11 @@
             width: 100,
         },
     ];
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-        data.push({
-            key: i,
-            commodity: `xx商品`,
-            city: '上海',
-            status: String(i % 2),
-            icon: '商品图标',
-            aaa: '商品货号',
-            通用名: '通用名',
-            商品分类: '商品分类',
-            unit: '基本单位',
-            specifications: '规格',
-            manufacturer: '生产厂家',
-
-        });
-    }
 
     export default {
         data(){
             return {
-                data,
+                data: [],
                 columns,
                 //  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
                 scroll: oneRowSearch(columns),
@@ -147,16 +130,18 @@
         methods: {
             //  主要请求
             searchFn(){
-                requestPrescriptionAuditPage(paginationEncode(this.pagination))
+                requestPrescriptionAuditPage(Object.assign({},
+                    { param: this.searchData },
+                    paginationEncode(this.pagination)
+                ))
                     .then(v => {
                         const { data } = v;
                         data.records.forEach((item, index) => {
                             item.key = index;
-                            item.createTime = item.createTime.substr(0, 10);
                         });
                         this.data = data.records;
+                        console.log(data.records);
                         this.pagination = paginationDecode(this.pagination, data);
-
                     });
             },
             //  展示的每一页数据变换
