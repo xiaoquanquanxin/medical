920c5d63b061f5f7844295857b5bb84a04d8db94
xiaoquanquanxin <xxkl123@126.com>
2020-10-24 19:08:35 +0800

    高度

--- src/api/task/configuration.ts
+++ src/api/task/configuration.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request';
 
-//	配置分页			✅，⚠️接口文档请求参数和原型不同
+//	配置分页			✅，⚠️接口文档请求参数和原型不同，这块原型有很多input和select
 export function requestPrescriptionConfigPzrw(data: any) {
 	return request({
 		url: '/api/prescription/config/pzrw',
