045ac51c7753ebed48cb07b7dc90c1188f298d11
xiaoquanquanxin <xxkl123@126.com>
2020-10-26 23:58:53 +0800

    高度

--- src/layout/layoutHistoryList/layoutHistoryList.vue
+++ src/layout/layoutHistoryList/layoutHistoryList.vue
@@ -16,10 +16,20 @@
                     :key="item.name"
                     v-for="(item , index) in historyRouteList"
             >
-                <router-link :to="{name:item.name}">
-                    <span>{{item.chName}}</span>
-                    <span @click="deleteHistory($event,item)">x</span>
-                </router-link>
+                <span>
+                    <router-link :to="{name:item.name}">
+                        <span>{{item.chName}}</span>
+                    </router-link>
+                </span>
+                <b @click="deleteHistory($event,item,index)"
+                   class="close-svg"
+                   v-if="historyRouteList.length>=2"
+                >
+                    <svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor"
+                         aria-hidden="true" focusable="false" class="">
+                        <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
+                    </svg>
+                </b>
             </a-menu-item>
         </a-menu>
     </div>
@@ -82,9 +92,13 @@
                 this.historyRouteList.push(routeItem);
             },
             //
-            deleteHistory($event, item){
-                debugger
-                console.log($event, item);
+            deleteHistory(e, item, index){
+                e.stopPropagation();
+                //  至少保留1个
+                if (this.historyRouteList.length < 2) {
+                    return;
+                }
+                this.historyRouteList.splice(index, 1);
             }
         }
     };
@@ -111,5 +125,11 @@
     /*未激活*/
     .nonactivated {
         color: grey;
+    }
+    
+    /*关闭*/
+    .close-svg {
+        color: grey !important;
+        vertical-align: middle;
     }
 </style>
