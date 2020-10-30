f63e920f983bdb5ea9d66a5e7bed3b9cf0cb079d
xiaoquanquanxin <xxkl123@126.com>
2020-10-30 18:34:50 +0800

    高度

---
+++ src/utils/scheme.ts
@@ -0,0 +1,51 @@
+//	删除处方模板
+import {requestPrescriptionTemplateDelete, requestPrescriptionTemplatePage} from "@/api/scheme/scheme"
+import {paginationDecode, paginationEncode} from "@/utils/pagination"
+
+//	删除某个处方
+export function deleteSchemeFn(sItem: { id: string, prescriptionName: string }) {
+	//	@ts-ignore
+	this.$confirm({
+		title: `确定删除${sItem.prescriptionName}`,
+		okText: '确认',
+		okType: 'danger',
+		cancelText: '取消',
+		onOk: () => {
+			return requestPrescriptionTemplateDelete(sItem.id)
+				.then(v => {
+					//	@ts-ignore
+					this.$message.success('操作成功');
+					//	@ts-ignore
+					this.searchFn();
+				})
+				.catch(v => {
+					//	@ts-ignore
+					this.$message.error('操作失败');
+				});
+		},
+		onCancel() {
+			console.log('取消');
+		},
+	});
+}
+
+//	获取处方列表数据
+export function getSchemeListFn() {
+	requestPrescriptionTemplatePage(Object.assign({},
+		//	@ts-ignore
+		this.searchData,
+		//	@ts-ignore
+		paginationEncode(this.pagination))
+	)
+		.then(v => {
+			const {data} = v;
+			data.records.forEach((item: any, index: number) => {
+				item.key = index;
+			});
+			//	@ts-ignore
+			this.data = data.records;
+			//	@ts-ignore
+			this.pagination = paginationDecode(this.pagination, data);
+		});
+}
+
