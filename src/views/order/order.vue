db195c853605d93e047fedf900c4f9c9f08bcd8f
xiaoquanquanxin <xxkl123@126.com>
2020-11-04 22:11:27 +0800

    菜单

--- src/views/order/order.vue
+++ src/views/order/order.vue
@@ -2,15 +2,17 @@
     <div class="layout-content-inner-main">
         <!--搜索相关-->
         <div class="a-input-group">
-            <a-input class="basic-input-width" v-model="searchData.orderNumber" placeholder="请输入订单号"/>
-            <a-input class="basic-input-width" v-model="searchData.prescriptionsNumber" placeholder="请输入处方单号"/>
+            <a-input class="basic-input-width" v-model="searchData.orderCode" placeholder="请输入订单号"/>
+            <a-input class="basic-input-width" v-model="searchData.prescriptionCode" placeholder="请输入处方单号"/>
             <a-input class="basic-input-width" v-model="searchData.patientName" placeholder="请输入患者姓名"/>
-            <a-select class="lengthen-select-width" v-model="searchData.status" placeholder="请选择支付状态">
-                <a-select-option value="1600">1600</a-select-option>
-                <a-select-option value="160">160</a-select-option>
+            <a-select class="lengthen-select-width" v-model="searchData.payStatus" placeholder="请选择支付状态">
+                <a-select-option value="0">待支付</a-select-option>
+                <a-select-option value="1">已支付</a-select-option>
             </a-select>
             <a-button class="basic-button-width" type="primary" @click="searchFn">搜索</a-button>
         </div>
+        <br>
+        <b>配置任务那里，订单号 === 处方单号，这里呢？</b>
         <!--表格-->
         <a-table
                 :columns="columns"
@@ -20,7 +22,7 @@
         >
             <div slot="operation" slot-scope="scope,sItem,sIndex,extra">
                 <a-space size="small">
-                    <router-link :to="{name:'orderDetail',params:{orderDetailId:'322323'}}">详情</router-link>
+                    <router-link :to="{name:'orderDetail',params:{orderDetailId:sItem.id}}">详情</router-link>
                 </a-space>
             </div>
         </a-table>
@@ -52,36 +54,37 @@
         onShowSizeChange
     } from '@/utils/pagination.ts';
     import { oneRowSearch } from '@/utils/tableScroll';
+    import { requestOrderPage } from '../../api/order/order';
 
     const columns = [
         {
             title: '订单号',
-            dataIndex: 'name',
-            width: 100,
+            dataIndex: 'orderCode',
+            width: 200,
         },
         {
             title: '处方单号',
-            dataIndex: 'aaa',
-            width: 100,
+            dataIndex: 'prescriptionCode',
+            width: 200,
         },
         {
             title: '医院名称',
-            dataIndex: 'bbb',
+            dataIndex: 'hospitalName',
             width: 100,
         },
         {
             title: '处方医生',
-            dataIndex: 'ccc',
+            dataIndex: 'doctorName',
             width: 100,
         },
         {
             title: '缴费金额',
-            dataIndex: 'coun',
+            dataIndex: 'amountPaid',
             width: 100,
         },
         {
             title: '支付状态',
-            dataIndex: 'dddd',
+            dataIndex: 'payStatus',
             width: 100,
         }, {
             title: '操作',
@@ -89,26 +92,11 @@
             scopedSlots: { customRender: 'operation' }
         },
     ];
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-        data.push({
-            key: i,
-            name: '许晓飞',
-            sex: 1,
-            familyGroup: 'xxx',
-            aaa: '处方单号',
-            bbb: '医院名称',
-            ccc: '处方医生',
-            coun: '缴费金额',
-            dddd: '支付状态',
-
-        });
-    }
     //  订单管理
     export default {
         data(){
             return {
-                data,
+                data: [],
                 columns,
                 //  搜索数据
                 searchData: {},//  设置横向或纵向滚动，也可用于指定滚动区域的宽和高
@@ -122,19 +110,18 @@
         methods: {
             //  主要请求
             searchFn(){
-//                requestChannelBusinessPage(paginationEncode(this.pagination))
-//                    .then(v => {
-//                        const { data } = v;
-//                        console.log(data);
-//                data.records.forEach((item, index) => {
-//                    item.key = index;
-//                    item.createTime = item.createTime.substr(0, 10);
-//                });
-//                        this.data = data.records;
-//                        this.pagination = paginationDecode(this.pagination, data);
-//                    });
+                requestOrderPage(Object.assign({}, this.searchData, paginationEncode(this.pagination)))
+                    .then(v => {
+                        const { data } = v;
+                        data.records.forEach((item, index) => {
+                            item.key = index;
+                        });
+                        this.data = data.records;
+                        this.pagination = paginationDecode(this.pagination, data);
+                        console.log(JSON.parse(JSON.stringify(this.data[0])));
+                    });
             },
-               onShowSizeChange,
+            onShowSizeChange,
             pageChange,
         }
     };
