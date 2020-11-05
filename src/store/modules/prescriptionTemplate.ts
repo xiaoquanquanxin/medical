d995368c12a1c18fe8b26992275e3d9a3892a655
xiaoquanquanxin <xxkl123@126.com>
2020-11-06 00:19:53 +0800

    菜单

--- src/store/modules/prescriptionTemplate.ts
+++ src/store/modules/prescriptionTemplate.ts
@@ -6,9 +6,9 @@ interface STATE {
 	//	肠内营养支持 - 选择的能量
 	// selectedEnergyId: number;
 	//	肠内营养支持 - 商品源的数据，⚠️⚠️⚠️⚠️⚠️⚠️️这个总是新的【纯的】，其他数据修改要做JSON.parse(JSON.stringify())
-	originCommodityList: [];
+	// originCommodityList: [];
 	//	勾选的时间数据，用于选择时间按钮传
-	timeOriginList: [],
+	// timeOriginList: [],
 	//	肠内营养支持 - 被选中的数据
 	// selectedCommodityList: [];
 
@@ -22,11 +22,11 @@ const state: STATE = {
 	//	肠内营养支持 - 选择的能量
 	// selectedEnergyId: 0,
 	//	肠内营养支持 - 商品源的数据
-	originCommodityList: [],
+	// originCommodityList: [],
 	//	肠内营养支持 - 被选中的数据
 	// selectedCommodityList: [],
 	//	勾选的时间数据
-	timeOriginList: [],
+	// timeOriginList: [],
 
 	//	备注
 	remark: '',
@@ -37,12 +37,12 @@ const mutations = {
 	// SET_SELECTED_ENERGY_ID: (state: STATE, selectedEnergyId: number) => {
 	// 	state.selectedEnergyId = selectedEnergyId;
 	// },
-	SET_ORIGIN_COMMODITY_LIST: (state: STATE, originCommodityList: []) => {
-		state.originCommodityList = originCommodityList;
-	},
-	SET_TIME_ORIGIN_LIST: (state: STATE, timeOriginList: []) => {
-		state.timeOriginList = timeOriginList;
-	},
+	// SET_ORIGIN_COMMODITY_LIST: (state: STATE, originCommodityList: []) => {
+	// 	state.originCommodityList = originCommodityList;
+	// },
+	// SET_TIME_ORIGIN_LIST: (state: STATE, timeOriginList: []) => {
+	// 	state.timeOriginList = timeOriginList;
+	// },
 	// SET_SELECTED_COMMODITY_LIST: (state: STATE, selectedCommodityList: []) => {
 	// 	state.selectedCommodityList = selectedCommodityList;
 	// },
@@ -60,12 +60,12 @@ const actions = {
 	// setSelectedEnergyId: ({commit}: COMMIT_INTERFACE<STATE>, selectedEnergyId: number) => {
 	// 	commit('SET_SELECTED_ENERGY_ID', selectedEnergyId);
 	// },
-	setOriginCommodityList: ({commit}: COMMIT_INTERFACE<STATE>, originCommodityList: number) => {
-		commit('SET_ORIGIN_COMMODITY_LIST', originCommodityList);
-	},
-	setTimeOriginList: ({commit}: COMMIT_INTERFACE<STATE>, timeOriginList: number) => {
-		commit('SET_TIME_ORIGIN_LIST', timeOriginList);
-	},
+	// setOriginCommodityList: ({commit}: COMMIT_INTERFACE<STATE>, originCommodityList: number) => {
+	// 	commit('SET_ORIGIN_COMMODITY_LIST', originCommodityList);
+	// },
+	// setTimeOriginList: ({commit}: COMMIT_INTERFACE<STATE>, timeOriginList: number) => {
+	// 	commit('SET_TIME_ORIGIN_LIST', timeOriginList);
+	// },
 	// setSelectedCommodityList: ({commit}: COMMIT_INTERFACE<STATE>, selectedCommodityList: number) => {
 	// 	commit('SET_SELECTED_COMMODITY_LIST', selectedCommodityList);
 	// },
