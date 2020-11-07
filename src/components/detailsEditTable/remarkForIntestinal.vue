51d9e1f2cac47e1a4bbb58119d7fd36a13ca676d
xiaoquanquanxin <xxkl123@126.com>
2020-11-07 20:28:07 +0800

    菜单

---
+++ src/components/detailsEditTable/remarkForIntestinal.vue
@@ -0,0 +1,45 @@
+<template>
+    <a-textarea
+            placeholder="请输入备注"
+            v-model="value"
+            @change="setIntestinalRemark($event.target.value)"
+            :style="style"
+    />
+</template>
+<script>
+    import { mapGetters, mapActions } from 'vuex';
+    //  处方模板管理
+    //  增加口服肠内补充方案 - 备注
+    export default {
+        computed: {
+            intestinalRemark(){
+                debugger
+                return this.$store.state.intervention.intestinalRemark;
+            },
+            style(){
+                return {
+                    //  minHeight: `${this.$store.state.intervention.rowForRemark * 54 - 16 * 2}px`
+                };
+            },
+        },
+        data(){
+            return {
+                value: this.intestinalRemark,
+            };
+        },
+        $watch: {
+            intestinalRemark(value){
+                this.value = value;
+            }
+        },
+        created(){
+            this.value = this.intestinalRemark;
+        },
+        methods: {
+            ...mapActions('intervention', [
+                //  设置商品列表数据
+                'setIntestinalRemark',
+            ]),
+        }
+    };
+</script>
---
+++ src/components/detailsEditTable/remarkForOral.vue
@@ -0,0 +1,46 @@
+<template>
+    <a-textarea
+            placeholder="请输入备注"
+            v-model="value"
+            @change="setOralRemark($event.target.value)"
+            :style="style"
+    />
+</template>
+<script>
+    import { mapGetters, mapActions } from 'vuex';
+    //  处方模板管理
+    //  增加口服肠内补充方案 - 备注
+    export default {
+        computed: {
+            oralRemark(){
+                return this.$store.state.intervention.oralRemark;
+            },
+            style(){
+                return {
+                    //  minHeight: `${this.$store.state.intervention.rowForRemark * 54 - 16 * 2}px`
+                };
+            },
+        },
+        data(){
+            return {
+                value: this.oralRemark,
+            };
+        },
+        $watch: {
+            oralRemark(value){
+                console.log(value);
+                this.value = value;
+            }
+        },
+        created(){
+            console.log(this.oralRemark);
+            this.value = this.oralRemark;
+        },
+        methods: {
+            ...mapActions('intervention', [
+                //  设置商品列表数据
+                'setOralRemark',
+            ]),
+        }
+    };
+</script>
