f1a99b4591dc941e631c684d4938994bc53d8928
xiaoquanquanxin <xxkl123@126.com>
2020-11-07 18:22:31 +0800

    菜单

--- src/views/userList/intervention/addIntervention.vue
+++ src/views/userList/intervention/addIntervention.vue
@@ -353,6 +353,7 @@
                     })
                     .catch(err => {
                         console.log(err);
+                        this.$message.error('操作失败');
                     });
             },
             ...mapActions('intervention', [
