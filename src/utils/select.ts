eea086234f0699b128c94598720cce596ae96890
xiaoquanquanxin <xxkl123@126.com>
2020-11-07 16:02:51 +0800

    菜单

--- src/utils/select.ts
+++ src/utils/select.ts
@@ -1,6 +1,6 @@
 //  展开
 export function dropdownVisibleChangeFn(value: boolean, no: number) {
-	console.log(value, no);
+	//	console.log(value, no);
 	//	@ts-ignore
 	this.selectSuffixIconMap[no] = value
 }
