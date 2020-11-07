cbac9db409a822e34e361b7be7c74b6f1a2f47f0
xiaoquanquanxin <xxkl123@126.com>
2020-11-07 11:53:26 +0800

    菜单

---
+++ src/components/suffixIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <span style="color:black;"
+          :class="selectSuffixIconMap[iconNum]?'suffix-icon':'suffix-icon suffix-icon-down'"
+    >⬇️</span>
+</template>
+<script>
+    import { selectSuffixIconMap } from '../utils/select';
+    //    选择框的下拉标志
+    export default {
+        props: ['iconNum'],
+        data(){
+            return {
+                selectSuffixIconMap,
+            };
+        },
+        created(){
+            if (this.selectSuffixIconMap[this.iconNum] === undefined) {
+                this.selectSuffixIconMap[this.iconNum] = false;
+            }
+        }
+    };
+</script>
+<style>
+    .suffix-icon {
+        transition: .3s;
+    }
+    
+    .suffix-icon.suffix-icon-down {
+        transform: rotateZ(-180deg);
+    }
+</style>
