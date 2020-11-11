ca1187af48c16450644aef3525d973694241d6c4
xiaoquanquanxin <xxkl123@126.com>
2020-11-11 14:54:13 +0800

    配置历史

--- src/store/modules/cost.ts
+++ src/store/modules/cost.ts
@@ -4,6 +4,8 @@ import {COMMIT_INTERFACE} from "@/store";
 interface STATE {
 	//	收计费被选中的对象
 	selectCostData: any;
+	//	收计费被选中的对象的id
+	selectCostId: number;
 	//	操作类型	0,缴费，1退款）
 	isRefund: 0 | 1,
 }
@@ -11,11 +13,15 @@ interface STATE {
 //	有id，就是编辑了
 const state: STATE = {
 	selectCostData: null,
+	selectCostId: 0,
 	isRefund: 1,
 }
 const mutations = {
-	SET_SELECT_COST_DATA: (state: STATE, selectCostData: number) => {
+	SET_SELECT_COST_DATA: (state: STATE, selectCostData: any) => {
 		state.selectCostData = selectCostData;
+	},
+	SET_SELECT_COST_ID: (state: STATE, selectCostId: number) => {
+		state.selectCostId = selectCostId;
 	},
 	SET_COST_TYPE: (state: STATE, isRefund: 0 | 1) => {
 		state.isRefund = isRefund;
@@ -25,6 +31,9 @@ const mutations = {
 const actions = {
 	setSelectCostData: ({commit}: COMMIT_INTERFACE<STATE>, selectCostData: number) => {
 		commit('SET_SELECT_COST_DATA', selectCostData);
+	},
+	setSelectCostId: ({commit}: COMMIT_INTERFACE<STATE>, selectCostId: number) => {
+		commit('SET_SELECT_COST_ID', selectCostId);
 	},
 	setCostType: ({commit}: COMMIT_INTERFACE<STATE>, isRefund: 0 | 1) => {
 		commit('SET_COST_TYPE', isRefund);
